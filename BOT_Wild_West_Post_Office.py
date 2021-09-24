292138955b65ac789482293a71dbfc9f69179d35
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-09-24 16:05:49 +0200

    Update BOT_Wild_West_Post_Office.py

--- BOT_Wild_West_Post_Office.py
+++ BOT_Wild_West_Post_Office.py
@@ -59,7 +59,7 @@ async def on_message(message):
         await message.delete()
         await DMChannel.send(user, embed=embed)
         await channel.send(embed=embed)
-        await message.author.add_roles(role)
+        await author.add_roles(role)
 
 #----------------------------------------------------------------------------------------COMMANDS-------------------------------------------------------------------------------------------------------------
 
