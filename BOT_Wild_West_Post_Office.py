b16c41fe24261655983e554db164e7628e6852f5
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-09-25 12:39:59 +0200

    Added changing activities

--- BOT_Wild_West_Post_Office.py
+++ BOT_Wild_West_Post_Office.py
@@ -32,7 +32,7 @@ async def status_change():
     status = random.choice(statuses)
     await client.change_presence(activity=discord.Game(name=status))
     sleep_time = random.randint(1800,3600)
-    print("Activity has been changed to: {}, and will be changed again after: {} seconds." .format(status, sleep_time))
+    print("Activity has been changed to: {}, and next change will be again after: {} seconds." .format(status, sleep_time))
     await asyncio.sleep(sleep_time)
 client.loop.create_task(status_change())
 
