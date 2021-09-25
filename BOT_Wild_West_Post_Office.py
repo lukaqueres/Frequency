f2b747bc6a091fc603e32e98c11d0ced999b6284
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-09-25 11:57:25 +0200

    Update BOT_Wild_West_Post_Office.py

--- BOT_Wild_West_Post_Office.py
+++ BOT_Wild_West_Post_Office.py
@@ -23,11 +23,11 @@ async def on_ready():
   
 async def status_change():
   await client.wait_until_ready()
-    statuses = [Red Dead Redemption 2", "Red Dead Redemption 1", "Red Dead Online", "Red Dead Revolwer" ]
-    while not client.is_closed():
-      status = random.choice(statuses)
-      await client.change_presence(status=discord.Status.online, activity=discord.Game(name=status))
-      await asyncio.sleep(10)
+  statuses = [Red Dead Redemption 2", "Red Dead Redemption 1", "Red Dead Online", "Red Dead Revolwer" ]
+  while not client.is_closed():
+    status = random.choice(statuses)
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name=status))
+    await asyncio.sleep(10)
 client.loop.create_task(change_status())
 client.run()
 @client.event #----------------------------------ANTY PHISHING-------------------------------------------------------------------------------------------------
