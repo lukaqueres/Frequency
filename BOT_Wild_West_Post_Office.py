632af06e81e4c310ee164b1c85f0653d5eded8b5
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-10-19 22:37:43 +0200

    Update BOT_Wild_West_Post_Office.py

--- BOT_Wild_West_Post_Office.py
+++ BOT_Wild_West_Post_Office.py
@@ -3,8 +3,8 @@ from os import getenv
 from dotenv import load_dotenv
 from random import randrange, randint
 from datetime import datetime, date, timedelta
-from discord import member, DMChannel, FFmpegPCMAudio
-from discord.ext import tasks, commands, TextChannel
+from discord import member, DMChannel, FFmpegPCMAudio, TextChannel
+from discord.ext import tasks, commands
 from discord.utils import get
 from youtube_dl import YoutubeDL
 from discord.ext.commands import has_permissions, MissingPermissions, bot
