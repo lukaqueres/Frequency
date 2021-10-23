636e56e9a24488579a00d330ef4de2f2a1feb859
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-10-23 16:40:05 +0200

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -14,9 +14,8 @@ now = datetime.now() + timedelta(hours=2)
 today = date.today()
 current_day = today.strftime("%d/%m/%Y")   #global current_day
 current_time = now.strftime("%H:%M:%S")    #global current_time
-client = discord.Client()
+
 load_dotenv()
-players = {}
 
 class Setup(commands.Cog):
   def __init__(self, client):
@@ -26,6 +25,44 @@ class Setup(commands.Cog):
   async def on_ready(self):
     print('Setup module loaded')
 
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+    global current_day
+    global current_time
+    print("\n Bot joined in guild: \" {} \" guild on \" {} {} \".".format(guild, current_time, current_day))
+    with open('data.json', 'r') as f:
+		  prefixes = json.load(f)
+      
+		prefixes[str(guild.id)] = '$'
+    
+    with open('data.json','w') as f:
+      json.dump(prefixes, f, indent=4)
+      
+  @commands.Cog.listener()
+  async def on_guild_remove(self, guild):
+    global current_day
+    global current_time
+    print("\n Bot removed from guild: \" {} \" guild on \" {} {} \".".format(guild, current_time, current_day))
+    with open('data.json', 'r') as f:
+		  prefixes = json.load(f)
+      
+		prefixes.pop(str(guild.id))
+    
+    with open('data.json','w') as f:
+      json.dump(prefixes, f, indent=4)
+    
+  @commands.command()
+  async def change_prefix(self, ctx, prefix):
+    global current_day
+    global current_time
+    print("\n Prefix changed in guild: \" {} \" guild on \" {} {} \".".format(ctx.message.guild, current_time, current_day))
+    with open('data.json', 'r') as f:
+		  prefixes = json.load(f)
+      
+		prefixes[str(ctx.message.guild.id)] = prefix
+    
+    with open('data.json','w') as f:
+      json.dump(prefixes, f, indent=4)
     
 def setup(client):
   client.add_cog(Setup(client))
