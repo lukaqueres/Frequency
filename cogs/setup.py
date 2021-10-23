2978b19584ea9a3668aa20d52d2e9a68f96a8a7c
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-10-23 12:23:43 +0200

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,1 +1,31 @@
+import discord, json, io, os, typing, requests, random, asyncio
+from os import getenv
+from dotenv import load_dotenv
+from ffmpeg import *
+from random import randrange, randint
+from datetime import datetime, date, timedelta
+from discord import member, DMChannel, FFmpegPCMAudio, TextChannel
+from discord.ext import tasks, commands
+from discord.utils import get
+from youtube_dl import *
+from discord.ext.commands import has_permissions, MissingPermissions, bot
 
+now = datetime.now() + timedelta(hours=2)
+today = date.today()
+current_day = today.strftime("%d/%m/%Y")   #global current_day
+current_time = now.strftime("%H:%M:%S")    #global current_time
+client = discord.Client()
+load_dotenv()
+players = {}
+
+class Setup(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+    
+  @commands.Cog.listener()
+  async def on_ready(self):
+    print('Setup module loaded')
+
+    
+def setup(client):
+  client.add_cog(Setup(client))
