7a463b3bdb0e4811318f43e112b34f489b41dc63
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-10-24 12:35:52 +0200

    Create functions.py

---
+++ data_base/functions.py
@@ -0,0 +1,11 @@
+import discord, json, io, os, typing, requests, random, asyncio
+from os import getenv
+from dotenv import load_dotenv
+from ffmpeg import *
+from random import randrange, randint
+from datetime import datetime, date, timedelta
+from discord import member, DMChannel, FFmpegPCMAudio, TextChannel
+from discord.ext import tasks, commands
+from discord.utils import get
+from youtube_dl import *
+from discord.ext.commands import has_permissions, MissingPermissions, bot
