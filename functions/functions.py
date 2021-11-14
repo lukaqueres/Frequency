877513db9d52c5baf62fcf73b1f9361a45f39241
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-14 12:12:26 +0100

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,1 +1,17 @@
+import discord, json, io, os, typing, requests, random, asyncio
+from os import getenv
+from dotenv import load_dotenv
+from ffmpeg import *
+from random import randrange, randint
+from datetime import datetime, date, timedelta
+from discord import member, DMChannel, FFmpegPCMAudio, TextChannel
+from discord.ext import tasks, commands
+from discord.utils import get
+from youtube_dl import *
+from discord.ext.commands import has_permissions, MissingPermissions, bot
 
+def get_prefix(client, message):
+	with open('data.json', 'r') as f:
+		prefixes = json.load(f)
+		
+	return prefixes[str(message.guild.id)]
