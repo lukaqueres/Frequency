6532b260f08e74202bae6e4148fa15b05e3fe1f7
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-14 12:36:38 +0100

    Update BOT_Wild_West_Post_Office.py

--- BOT_Wild_West_Post_Office.py
+++ BOT_Wild_West_Post_Office.py
@@ -10,7 +10,7 @@ from discord.utils import get
 from youtube_dl import *
 from discord.ext.commands import has_permissions, MissingPermissions, bot
 #from youtube_dl import YoutubeDL
-from doable import *
+from functions/doable import *
 
 client = commands.Bot(command_prefix = get_prefix)
 
