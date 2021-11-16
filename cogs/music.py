6c9ff4273012a43abc1d5cec6ccc1f85943fd24e
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-16 17:03:54 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -17,6 +17,27 @@ load_dotenv()
 players = {}
 voice_clients = {}
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
 class Music(commands.Cog):
   def __init__(self, client):
     self.client = client
