a874aa1bbc42f63c59b7e16dfa6ae2431282e060
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-16 10:55:01 +0100

    Update spam_detection.py

--- cogs/spam_detection.py
+++ cogs/spam_detection.py
@@ -10,12 +10,7 @@ from discord.utils import get
 from youtube_dl import *
 from discord.ext.commands import has_permissions, MissingPermissions, bot
 
-
-def get_prefix(client, message):
-	with open('data.json', 'r') as f:
-		prefixes = json.load(f)
-		
-	return prefixes[str(message.guild.id)]
+from functions import get_prefix
 
 now = datetime.now() + timedelta(hours=2)
 today = date.today()
