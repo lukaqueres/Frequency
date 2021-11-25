6b9d0bf1bc58405f2e2d1905507a6c6c8f2a83b7
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-25 13:10:37 +0100

    Update management.py

--- cogs/management.py
+++ cogs/management.py
@@ -1,1 +1,26 @@
+import discord, json, io, os, typing, requests, random, asyncio
+from os import getenv
+from dotenv import load_dotenv
+from ffmpeg import *
+from random import randrange, randint
+from datetime import datetime, date, timedelta
+from discord import member, DMChannel, FFmpegPCMAudio, TextChannel
+from discord.ext import tasks, commands
+from discord.utils import get
+from youtube_dl import *
+from discord.ext.commands import has_permissions, MissingPermissions, bot
 
+from functions import get_prefix, get_time
+
+load_dotenv()
+
+class Management(commands.Cog):
+  def __init__(self, client):
+  	self.client = client
+    
+  @commands.Cog.listener()
+  async def on_ready(self):
+    print('Management module loaded')
+    
+def setup(client):
+  client.add_cog(Management(client))
