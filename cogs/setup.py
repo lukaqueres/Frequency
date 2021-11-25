bf1c4969688fc2f028ec9691f25e6eae3db5754d
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2021-11-25 15:56:07 +0100

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -65,16 +65,15 @@ class Setup(commands.Cog):
     embed.add_field(name="$play [ url / słowa kluczowe ]", value="Odtwarza muzykę na kanle głosowym na podstawie adresu url, lub słów kluczowych. Wymaga aby użytkownik znajdował się na kanale głosowym.", inline=False)
     embed.add_field(name="$stop", value="Zatrzymuje odtwarzanie oraz wychodzi z kanału głosowego.", inline=True)
     embed.add_field(name="$volume [ liczba ]", value="Zmienia głośność odtwarzania muzyki na kanale głosowym na określony procent ( domyślnie 50% ).", inline=True)
+    if (message.author.guild_permissions.manage_messages):
+      embed.add_field(name="Zarządzanie wiadomościami", value="Dostępne jedynie dla użytkowników z uprawnieniem zarządzania wiadomościami.", inline=False)
+      embed.add_field(name="$clear [ liczba ]", value="Usuwa określoną liczbę wiadomości z kanału ( nie licząc wiadomości z komendą ).", inline=True)
     if (message.author.guild_permissions.ban_members):
-      embed.add_field(name="", value="", inline=False)
       embed.add_field(name="Zarządzanie użytkownikami", value="Dostępne jedynie dla użytkowników z odpowiednimi uprawnieniami.", inline=False)
-      embed.add_field(name="", value="", inline=False)
       embed.add_field(name="$ban [ użytkownik ]", value="Nakłada bana na użytkownika.", inline=True)
       embed.add_field(name="$unban [ użytkownik ]", value="Usuwa bana z użytkownika, jeżeli go posiada.", inline=True)
     if (message.author.guild_permissions.administrator):
-      embed.add_field(name="", value="", inline=False)
       embed.add_field(name="Zarządzanie serwerem", value="Dostępne jedynie dla użytkowników z uprawnieniami administratora.", inline=False)
-      embed.add_field(name="", value="", inline=False)
       embed.add_field(name="$prefix_change [ prefix]", value="Zmienia prefiks serwera z którego korzysta bot. **UWAGA** działa jedynie przez krótki okres.", inline=False)
     msg = await ctx.send(embed=embed)
     await msg.add_reaction(':ballot_box_with_check:')
