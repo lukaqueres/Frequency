f346d11cb4f6d0888d7635729a79ac7a80a74f6b
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-12 18:25:42 +0100

    Update database_maintenance.py

--- cogs/database_maintenance.py
+++ cogs/database_maintenance.py
@@ -34,7 +34,7 @@ class Database_maintenance(commands.Cog):
     print("\n Bot joined in guild: \" {} \" guild on \" {} \".".format(guild, get_time()))
     default_prefix = '$'
     date_of_join = str("{") + get_time("DD") + str("}")
-    cur.execute("INSERT INTO SERVERS_PROPERTIES ( GUILD_ID, GUILD_NAME, DATE_OF_JOIN, GUILD_PREFIX, NUMBER_OF_USERS, ANTY_SPAM_FEATURE, ECONOMY, MUSIC, UPDATES) VALUES ('{}', '{}', '/{{}/}', '{}', '{}', '{}', '{}', '{}', '{}')".format(guild.id, guild.name, date_of_join, default_prefix, guild.member_count, "NO", "NO", "YES", "NO"));
+    cur.execute("INSERT INTO SERVERS_PROPERTIES ( GUILD_ID, GUILD_NAME, DATE_OF_JOIN, GUILD_PREFIX, NUMBER_OF_USERS, ANTY_SPAM_FEATURE, ECONOMY, MUSIC, UPDATES) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(guild.id, guild.name, date_of_join, default_prefix, guild.member_count, "NO", "NO", "YES", "NO"));
     con.commit()
 #
 #<----------> On Bot remove from guild <----------------------------------------------------------------->
