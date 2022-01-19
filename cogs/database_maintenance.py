bcc7a48826da56376119f47fa8e67f8e10807c55
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-19 20:47:57 +0100

    Update database_maintenance.py

--- cogs/database_maintenance.py
+++ cogs/database_maintenance.py
@@ -17,7 +17,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = get_prefix)
+client = discord.Client
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 con = psycopg2.connect(DATABASE_URL)
