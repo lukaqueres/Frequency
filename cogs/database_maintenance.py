c83e6ede86181fd73e6402d1cac49808df3e2bf5
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-22 19:40:07 +0100

    Update database_maintenance.py

--- cogs/database_maintenance.py
+++ cogs/database_maintenance.py
@@ -18,7 +18,7 @@ intents = discord.Intents.default()
 intents.members = True
 
 #client = commands.Bot(command_prefix = get_prefix, intents=intents)
-client = Bot
+client = discord.ext.commands.Bot
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 con = psycopg2.connect(DATABASE_URL)
