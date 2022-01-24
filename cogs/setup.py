25e6555a9cb23df0f77b179ff4a114eb5d92105f
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-24 21:43:26 +0100

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -14,6 +14,11 @@ from functions import get_prefix, get_time
 
 load_dotenv()
 
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = get_prefix, intents=intents)
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 con = psycopg2.connect(DATABASE_URL)
 cur = con.cursor()
