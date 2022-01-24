b4c3098b1afd25c15ae1ea2ddd65301d7d1a44f5
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-24 17:16:32 +0100

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -50,15 +50,15 @@ class Information(commands.Cog):
 			)
 		embed.set_thumbnail(url=user.avatar_url)
       
-		embed.add_field( name="Name", value=str(user), inline=True),
-		embed.add_field( name="ID", value=user.id, inline=True),
-		embed.add_field( name="Bot?", value=user.bot, inline=True),
-		embed.add_field( name="Top role", value=user.top_role.mention, inline=True),
-		embed.add_field( name="Status", value=str(user.status).title(), inline=True),
-		embed.add_field( name="Activity", value=f"{str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}", inline=True),
-		embed.add_field( name="Created at", value=user.created_at.strftime("%d/%m/%Y %H:%M:%S"), inline=True),
-		embed.add_field( name="Joined at", value=user.joined_at.strftime("%d/%m/%Y %H:%M:%S"), inline=True),
-		embed.add_field( name="Boosted", value=bool(user.premium_since), inline=True)
+		embed.add_field( name="Name:", value=str(user), inline=True),
+		embed.add_field( name="ID:", value=user.id, inline=True),
+		embed.add_field( name="Bot:", value=user.bot, inline=False),
+		embed.add_field( name="Top role:", value=user.top_role.mention, inline=True),
+		#embed.add_field( name="Status", value=str(user.status).title(), inline=True), <- FIX IT
+		#embed.add_field( name="Activity", value=f"{str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}", inline=True), <- FIX IT
+		embed.add_field( name="Created at:", value=user.created_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False),
+		embed.add_field( name="Joined at:", value=user.joined_at.strftime("%d/%m/%Y %H:%M:%S"), inline=True),
+		embed.add_field( name="Boosted:", value=bool(user.premium_since), inline=True)
 
 		await ctx.send(embed=embed)
 	"""@info.error
