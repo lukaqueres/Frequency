13e3f1daa7cea49bfdf606a7fedf687cc5081f46
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-24 16:40:03 +0100

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -30,7 +30,8 @@ class Information(commands.Cog):
 	@commands.command()
 	async def info(self, ctx, value = 'default', secondary_value : discord.Member=None): # secondary_value : discord.Member=None <- for specified type of input
 		if secondary_value.isnumeric():
-			secondary_value = ( "<@" + secondary_value + ">" )
+			#secondary_value = ( "<@" + secondary_value + ">" )
+			secondary_value = await bot.fetch_member(secondary_value)
 		if value == 'default': # You can't pick nothin' do ya? 
 			await ctx.send("You have to specify what kind of information you want!")
 			return 0
