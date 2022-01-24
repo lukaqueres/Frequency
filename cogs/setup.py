ba6c6a83c209438c03b986a45efd4698fa61116d
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-24 19:56:51 +0100

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -66,7 +66,7 @@ class Setup(commands.Cog):
 #
 
 	@commands.command()
-	async def set(self, ctx, task = 'default', value = 'default', value_two : Channel=None):
+	async def set(self, ctx, task = 'default', value = 'default', value_two : discord.Channel=None):
 		message = ctx.message
 		guild = ctx.guild
 		guild_id = guild.id
