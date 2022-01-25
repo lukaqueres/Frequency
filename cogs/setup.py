4aa8bd77d8ec912620cd06c8c6f6717c5b44a436
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-25 20:51:36 +0100

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -18,7 +18,7 @@ intents = discord.Intents().all()
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = get_prefix, intents=intents)
+client = commands.client(command_prefix = get_prefix, intents=intents)
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 con = psycopg2.connect(DATABASE_URL)
@@ -125,7 +125,7 @@ class Setup(commands.Cog):
 						value_two = value_two.replace(i, "")
 					print("ID: {}".format(value_two))
 					#channel = discord.utils.get(guild.channels, id=value_two, type="ChannelType.text") 
-					channel = self.client.guild.get_channel(value_two)
+					channel = self.bot.guild.get_channel(value_two)
 					#channel = client.get_channel(value_two)
 					#channel = discord.utils.get(ctx.guild.channels, id=value_two)
 					#channel = ctx.get_channel(value_two)
