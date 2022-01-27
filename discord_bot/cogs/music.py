9f4e830fcc562b656b70001f7fcdbf7439ab51d8
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-27 20:10:31 +0100

    Update music.py

--- discord_bot/cogs/music.py
+++ discord_bot/cogs/music.py
@@ -28,9 +28,9 @@ ytdl_format_options = {
 	'no_warnings': True,
 	'default_search': 'auto',
 	'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes
-	'force-ipv4': True,
-	'extract_flat': True, 
-	'skip_download': True
+	'force-ipv4': True #,
+	#'extract_flat': True, 
+	#'skip_download': True
 }
 
 ffmpeg_options = {
