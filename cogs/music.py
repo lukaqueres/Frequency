6a88aa8a3099df088e1eba218318c596a5fa0a8a
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-27 09:13:03 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -28,6 +28,7 @@ ytdl_format_options = {
 	'no_warnings': True,
 	'default_search': 'auto',
 	'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+	'force-ipv4': True,
 }
 
 ffmpeg_options = {
