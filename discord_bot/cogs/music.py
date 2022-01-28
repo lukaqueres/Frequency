7908f9c8aa6507174feb41c04de32c6b83891914
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-28 11:36:43 +0100

    Update music.py

--- discord_bot/cogs/music.py
+++ discord_bot/cogs/music.py
@@ -99,6 +99,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 			id_list = []
 			for item in data['entries']:
     				id_list.append(item['id'])
+				play(ctx, item['id'])
 			print(f"Liczba id: {len(id_list)}")
 			print(f"Lista id: {id_list}")
 			data = data['entries'][0]
