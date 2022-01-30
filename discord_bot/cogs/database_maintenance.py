32e52b693d37614ae8d3026017a01b50eae4fa1d
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-30 12:55:39 +0100

    Update database_maintenance.py

--- discord_bot/cogs/database_maintenance.py
+++ discord_bot/cogs/database_maintenance.py
@@ -121,14 +121,14 @@ class Database_maintenance(commands.Cog):
 		channel_id = channel.id
 		database_record_channel_one = get_database_data('servers_data', 'anty_spam_channel_id', guild_id)
 		database_record_channel_two = get_database_data('servers_data', 'updates_channel_id', guild_id)
+		null = None
 		if (channel_id == database_record_channel_one):
-			cur.execute("DELETE FROM servers_data anty_spam_channel_id WHERE GUILD_ID = '{}'".format(guild_id))
+			cur.execute("UPDATE servers_data SET updates_channel_id = '{}' WHERE GUILD_ID = '{}'".format(null, guild_id))
 			con.commit()
 			cur.execute("UPDATE servers_properties SET anty_spam_feature = '{}' WHERE GUILD_ID = '{}'".format('NO', guild_id))
 			con.commit()
 		elif (channel_id == database_record_channel_two):
-			cur.execute("DELETE FROM servers_data anty_spam_channel_id WHERE GUILD_ID = '{}'".format(guild_id))
-			#cur.execute("UPDATE servers_data SET updates_channel_id = '{}' WHERE GUILD_ID = '{}'".format('null', guild_id))
+			cur.execute("UPDATE servers_data SET updates_channel_id = '{}' WHERE GUILD_ID = '{}'".format(null, guild_id))
 			con.commit()
 			cur.execute("UPDATE servers_properties SET updates = '{}' WHERE GUILD_ID = '{}'".format('NO', guild_id))
 			con.commit()
