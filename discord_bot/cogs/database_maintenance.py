9b845b4bc9fecfc96576ea86fa03f4e092b0be49
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-30 12:07:53 +0100

    Update database_maintenance.py

--- discord_bot/cogs/database_maintenance.py
+++ discord_bot/cogs/database_maintenance.py
@@ -123,14 +123,14 @@ class Database_maintenance(commands.Cog):
 		database_record_channel_two = get_database_data('servers_data', 'updates_channel_id', guild_id)
 		if (channel_id == database_record_channel_one):
 			cur.execute("UPDATE servers_data SET anty_spam_channel_id = '{}' WHERE GUILD_ID = '{}'".format(None, guild_id))
-    	con.commit()
+			con.commit()
 			cur.execute("UPDATE servers_properties SET anty_spam_feature = '{}' WHERE GUILD_ID = '{}'".format('NO', guild_id))
-    	con.commit()
+			con.commit()
 		elif (channel_id == database_record_channel_two):
 			cur.execute("UPDATE servers_data SET updates_channel_id = '{}' WHERE GUILD_ID = '{}'".format(None, guild_id))
-    	con.commit()
+			con.commit()
 			cur.execute("UPDATE servers_properties SET updates = '{}' WHERE GUILD_ID = '{}'".format('NO', guild_id))
-    	con.commit()
+			con.commit()
 		else:
 			return 0
     
