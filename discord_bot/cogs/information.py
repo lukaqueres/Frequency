1b8e5892d7a7df086edb49b65b4a1a40c6d9aff3
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-31 17:08:58 +0100

    Update information.py

--- discord_bot/cogs/information.py
+++ discord_bot/cogs/information.py
@@ -58,13 +58,13 @@ class Information(commands.Cog):
 				)
 			embed.set_thumbnail(url=user.avatar_url)
       
-			embed.add_field( name=chr(173), value=f"**User**:{str(user)}\n**User ID**: {user.id}", inline=True),
-			embed.add_field( name=chr(173), value=f"**Created**:{account_created}\n**Joined**: {guild_join}", inline=True),
+			embed.add_field( name=chr(173), value=f"**User**: {str(user)}\n**User ID**: {user.id}", inline=True),
+			embed.add_field( name=chr(173), value=f"**Created**: {account_created}\n**Joined**: {guild_join}", inline=True),
 			#embed.add_field(name = chr(173), value = chr(173), inline=False)
 			embed.add_field( name="All roles:", value=roles, inline=False),
-			embed.add_field(name = chr(173), value = f"**Status**:{str(user.status).title()}\n**Activity**: {str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}\n**Bot**: {'NO' if not user.bot else 'YES'}", inline=True),
+			embed.add_field(name = chr(173), value = f"**Status**: {str(user.status).title()}\n**Activity**: {str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}\n**Bot**: {'NO' if not user.bot else 'YES'}", inline=True),
 			embed.add_field( name= chr(173), value=f"**Top role**: {user.top_role.mention}\n**Number of roles**: {len(rolelist)}", inline=True),
-			embed.add_field(name = chr(173), value = chr(173), inline=False)
+			#embed.add_field(name = chr(173), value = chr(173), inline=False)
 			#embed.add_field( name="Bot:", value=user.bot, inline=True),
 			#embed.add_field( name="Status", value=str(user.status).title(), inline=True), <- FIX IT
 			#embed.add_field( name="Activity", value=f"{str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}", inline=True), <- FIX IT
