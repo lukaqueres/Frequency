ec79f26adbae9d28411332a7cb9948d21e537993
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-01-31 11:18:20 +0100

    Update spam_detection.py

--- discord_bot/cogs/spam_detection.py
+++ discord_bot/cogs/spam_detection.py
@@ -17,82 +17,82 @@ client = commands.Bot(command_prefix = get_prefix)
 load_dotenv()
 
 class Message_processing:
-  def __init__(self, ctx):
+	def __init__(self, ctx):
 		self.bot = ctx.bot
 		self._guild = ctx.guild
 		self._channel = ctx.channel
 
 class Spam_detection(commands.Cog):
-  def __init__(self, client):
-  	self.client = client
+	def __init__(self, client):
+		self.client = client
     
-  @commands.Cog.listener()
-  async def on_ready(self):
-    print('Spam detection module loaded')
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print('Spam detection module loaded')
     
-  @commands.Cog.listener()
-  async def on_message(self, message):
-    #await client.process_commands(message)
-    guild_id = message.guild.id
-    database_record = get_database_data('servers_properties', 'message_check_feature', guild_id)
-    black_listed = ['Gift', 'gift', 'Steam', 'steam', 'Free', 'free', 'Nitro', 'nitro', 'Discord', 'discord', 'giveaway', 'Giveaway', 'Skin', 'skin', 'CS:GO', 'Counter-Strike: Global Offensive', 'CS']
-    black_listed_length = (len(black_listed))
-    black_listed_words_number_detected = 0
-    if database_record == 'NO':
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		#await client.process_commands(message)
+		guild_id = message.guild.id
+		database_record = get_database_data('servers_properties', 'message_check_feature', guild_id)
+		black_listed = ['Gift', 'gift', 'Steam', 'steam', 'Free', 'free', 'Nitro', 'nitro', 'Discord', 'discord', 'giveaway', 'Giveaway', 'Skin', 'skin', 'CS:GO', 'Counter-Strike: Global Offensive', 'CS']
+		black_listed_length = (len(black_listed))
+		black_listed_words_number_detected = 0
+		if database_record == 'NO':
+			return 0
+		alert_channel_id = get_database_data('servers_data', 'message_check_channel_id', guild_id)
+		alert_channel = self.client.get_channel( id = alert_channel_id )
+		if (message.author == client.user):
+			return 0
+		if (('http://' in message.content ) or ('https://' in message.content)):
+			for x in black_listed:
+				if (x in message.content):
+					black_listed_words_number_detected += 1
+			if (black_listed_words_number_detected == 2):
+				message_state = 'Define'
+				try:
+					await message.delete()
+					message_state = 'deleted'
+					#print("Message deleted")
+				except commands.errors.MessageNotFound:
+					message_state = 'not found'
+				except discord.Forbidden:
+					message_state = 'not deleted due to not enough permissions'
+				except:
+					#print("Message not deleted")
+					message_state = 'not deleted'
+				print("\nPosible scam by: \" {} \" on: \" {} \" channel in: \" {} \" guild on \" {} \". Message {}.".format(message.author, message.channel, message.guild, get_time(), message_state))
+				message_state = message_state.capitalize()
+				message_content = ('`' + message.content + '`')
+				message_words = []
+				message_words = message.content.split() 
+				link = []
+				nolinks = 0
+				for i in message_words:
+					if  ( 'http://' in i ) or ( 'https://' in i ):
+						nolinks = nolinks + 1
+						link.append('||`' + i + '`||')
+						#print(f"link0: {link}")
+				embed = discord.Embed( 
+					title="Message flagged",
+					description=" ",
+					color=0x0000ff,
+					timestamp=datetime.utcnow() + timedelta( hours = 0 ) #timestamp=datetime.datetime.utcnow() + timedelta( hours = 1 )
+				)
+				embed.add_field(name= chr(173), value=f"**User**: {message.author} \n**User ID**: {message.author.id}", inline=True),
+				embed.add_field(name= chr(173), value=f"**Channel**: {message.channel} \n**Channel ID**: {message.channel.id}", inline=True),
+				embed.add_field(name= chr(173), value=chr(173), inline=False),
+				embed.add_field(name="Message content:", value=message_content, inline=True),
+				if nolinks == 1:
+					embed.add_field(name="Link:", value=link[0], inline=True),
+				else:
+					links = ''.join(str(x + ', ') for x in link)
+					embed.add_field(name="Links:", value=links, inline=True),
+				embed.add_field(name=chr(173), value=f"**Message status**: {message_state}", inline=False),
+				embed.set_footer(text="Provided by Wild West Post Office")
+				await alert_channel.send(embed=embed)
+	else: 
       return 0
-    alert_channel_id = get_database_data('servers_data', 'message_check_channel_id', guild_id)
-    alert_channel = self.client.get_channel( id = alert_channel_id )
-    if (message.author == client.user):
-      return
-    if (('http://' in message.content ) or ('https://' in message.content)):
-      for x in black_listed:
-        if (x in message.content):
-          black_listed_words_number_detected += 1
-          if (black_listed_words_number_detected == 2):
-            message_state = 'Define'
-            try:
-              await message.delete()
-              message_state = 'deleted'
-              print("Message deleted")
-            except commands.errors.MessageNotFound:
-              message_state = 'not found'
-            except discord.Forbidden:
-              message_state = 'not deleted due to not enough permissions'
-            except:
-              print("Message not deleted")
-              message_state = 'not deleted'
-            print("\nPosible scam by: \" {} \" on: \" {} \" channel in: \" {} \" guild on \" {} \". Message {}.".format(message.author, message.channel, message.guild, get_time(), message_state))
-            message_state = message_state.capitalize()
-            message_content = ('`' + message.content + '`')
-            message_words = []
-            message_words = message.content.split() 
-            link = []
-            nolinks = 0
-            for i in message_words:
-              if  ( 'http://' in i ) or ( 'https://' in i ):
-                nolinks = nolinks + 1
-                link.append('||`' + i + '`||')
-                #print(f"link0: {link}")
-            embed = discord.Embed( 
-              title="Message flagged",
-              description=" ",
-              color=0x0000ff,
-              timestamp=datetime.utcnow() + timedelta( hours = 0 ) #timestamp=datetime.datetime.utcnow() + timedelta( hours = 1 )
-            )
-            embed.add_field(name= chr(173), value=f"**User**: {message.author} \n**User ID**: {message.author.id}", inline=True),
-            embed.add_field(name= chr(173), value=f"**Channel**: {message.channel} \n**Channel ID**: {message.channel.id}", inline=True),
-            embed.add_field(name= chr(173), value=chr(173), inline=False),
-            embed.add_field(name="Message content:", value=message_content, inline=True),
-            if nolinks == 1:
-              embed.add_field(name="Link:", value=link[0], inline=True),
-            else:
-              links = ''.join(str(x + ', ') for x in link)
-              embed.add_field(name="Links:", value=links, inline=True),
-            embed.add_field(name=chr(173), value=f"**Message status**: {message_state}", inline=False),
-            embed.set_footer(text="Provided by Wild West Post Office")
-            await alert_channel.send(embed=embed)
-    else: 
-      return
     
 def setup(client):
-  client.add_cog(Spam_detection(client))
+	client.add_cog(Spam_detection(client))
