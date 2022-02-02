e5546d8b3d532096f800cb9e70371da0667c72e0
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-02 21:59:20 +0100

    Update music.py

--- discord_bot/cogs/music.py
+++ discord_bot/cogs/music.py
@@ -474,7 +474,7 @@ class Music(commands.Cog):
 		if ctx.voice_client:
 			if ctx.voice_client.channel != ctx.author.voice.channel:
 				raise commands.CommandError('Bot is already in a voice channel.')
-				return await ctx.send('Bot is already in voice channel.'
+				return await ctx.send('Bot is already in voice channel.')
 
 def setup(client):
 	client.add_cog(Music(client))
