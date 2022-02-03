e411cc617414dfdf68e19949900da9b2d027988f
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-03 14:22:56 +0100

    Update message_check.py

--- discord_bot/cogs/message_check.py
+++ discord_bot/cogs/message_check.py
@@ -128,7 +128,7 @@ class Message_check(commands.Cog):
 			color= message.author.colour,
 			timestamp=datetime.utcnow() + timedelta( hours = 0 ) #timestamp=datetime.datetime.utcnow() + timedelta( hours = 1 )
 		)
-		embed.set_thumbnail(url=deleter.icon_url if modDeleted else message.author.icon_url)
+		embed.set_thumbnail(url=deleter.icon_url if modDeleted else message.author.avatar_url)
 		embed.add_field(name= chr(173), value=f"**Message author**: {message.author} \n**Message author ID**: {message.author.id}", inline=True),
 		embed.add_field(name= chr(173), value=f"**Deleter**: {deleter if modDeleted else message.author} \n**Deleter ID**: {deleter.id if modDeleted else message.author,id}", inline=True),
 		embed.add_field(name= chr(173), value=f"**Channel**: {message.channel} \n**Channel ID**: {message.channel.id}", inline=True),
@@ -150,7 +150,7 @@ class Message_check(commands.Cog):
 			color= message.author.colour,
 			timestamp=datetime.utcnow() + timedelta( hours = 0 ) #timestamp=datetime.datetime.utcnow() + timedelta( hours = 1 )
 		)
-		embed.set_thumbnail(url=message.author.icon_url)
+		embed.set_thumbnail(url=message.author.avatar_url)
 		embed.add_field(name= chr(173), value=f"**Message author**: {message.author} \n**Message author ID**: {message.author.id}", inline=True),
 		embed.add_field(name= chr(173), value=f"**Channel**: {message.channel} \n**Channel ID**: {message.channel.id}", inline=True),
 		embed.add_field(name= 'Message content before:', value=message_before.content, inline=False),
