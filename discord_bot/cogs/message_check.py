cba5907446730044d08fbd0407115ab2f6cbd8ec
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-03 18:08:19 +0100

    Update message_check.py

--- discord_bot/cogs/message_check.py
+++ discord_bot/cogs/message_check.py
@@ -119,6 +119,8 @@ class Message_check(commands.Cog):
 		deleter = None
 		if (message.author.bot):
 			return 0
+		if message.content.startswith('$say'):
+			return 0
 		async for entry in message.guild.audit_logs(limit=1,action=discord.AuditLogAction.message_delete):
 			if entry.created_at.now() == datetime.utcnow():
 				deleter = entry.user
