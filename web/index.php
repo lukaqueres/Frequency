d4de4e256f734cb58d527ddd683d81b46767c662
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-04 11:31:05 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
-</body>
-</html> 
-// <?php include_once("home.html"); ?>
+<?php include_once("home.html"); ?>
