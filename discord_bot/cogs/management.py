aa14bfdfa12563ef5c2c98de50963c6b7f724933
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-10 19:12:14 +0100

    Update management.py

--- discord_bot/cogs/management.py
+++ discord_bot/cogs/management.py
@@ -24,13 +24,13 @@ class Management(commands.Cog):
   async def on_ready(self):
     print('Management module loaded')
   
-  @commands.command(name='ban', brief='Ban specified user.', description='Bans selected member. It is possible to ban without reason, but you can still provide it.'))
+  @commands.command(name='ban', brief='Ban specified user.', description='Bans selected member. It is possible to ban without reason, but you can still provide it.')
   @commands.has_permissions(ban_members = True)
   async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
     print("Member {member} banned on {ctx.guild} guild on {get_time()} .")
 
-  @commands.command(name='ban', brief='Clear ban specified user.', description='Un-bans selected member. Remember that you can\'t un-ban nto banned user.'))
+  @commands.command(name='ban', brief='Clear ban specified user.', description='Un-bans selected member. Remember that you can\'t un-ban nto banned user.')
   @commands.has_permissions(ban_members = True)
   async def unban(ctx, *, member):
     banned_users = await ctx.guild.bans()
