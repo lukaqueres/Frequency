e96dae57812989b69d5cd7b2dee03306787dcf2c
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-12 11:45:03 +0100

    Update message_check.py

--- discord_bot/cogs/message_check.py
+++ discord_bot/cogs/message_check.py
@@ -120,9 +120,9 @@ class Message_check(commands.Cog):
 		task = task.lower()
 		key_value = key_value.lower()
 		if task == 'set':
-			key_words = key_value.split
-			print(f'Key-words: {key_words}')
-			print(f'Key-value: {key_value}')
+			key_words = key_value.split()
+			#print(f'Key-words: {key_words}')
+			#print(f'Key-value: {key_value}')
 			if len(key_words) > 20:
 				return ctx.send("Total number of key-words must be less or equal 20")
 			elif len(value) < 1:
