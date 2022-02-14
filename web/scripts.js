cbf99e391386f489490e95cff39c37fb322c59c3
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-14 08:20:39 +0100

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -11,3 +11,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+function animate(id) {
+    var x = document.getElementById(id);
+    if (x.classList.contains("hide")) {
+      x.classList.remove("hide");
+    } else {
+      x.classList.add("hide");
+    }
+}
