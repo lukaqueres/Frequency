e4cf90f0399d136ff6da97df8a80e89729a157f8
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-14 07:50:37 +0100

    Create scripts.js

---
+++ web/scripts.js
@@ -0,0 +1,13 @@
+function scrollFunction(mybutton) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
