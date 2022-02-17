6e3ecd81bfae2b858c47e2c1e997d16956f2fbad
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-17 12:07:18 +0100

    Update manageDB.php

--- web/manageDB.php
+++ web/manageDB.php
@@ -1,8 +1,10 @@
 <?php
 // This function reads your DATABASE_URL config var and returns a connection[
 // string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
+function pg_connection_string_from_database_url() 
+{
   extract(parse_url($_ENV["DATABASE_URL"]));
+  
   return "user=$user password=$pass sslmode=require host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
@@ -12,16 +14,40 @@ class DatabaseManager {
 
   function get_dbmembers($pg_conn)
     {   
-      $members = pg_query($pg_conn, "SELECT SUM(number_of_members) as total FROM servers_properties");
+      $members = pg_query($pg_conn, "SELECT SUM(number_of_members) as members FROM servers_properties");
       $result = pg_fetch_assoc($members);
-      return $result['total'];
+      
+      $result = add_zeros($result['members'], 5)
+      return $result;
     }
   
   function get_dbguilds($pg_conn)
     {
       $guilds = pg_query($pg_conn, "SELECT COUNT(*) as guilds FROM servers_properties");
       $result = pg_fetch_assoc($guilds);
-      return $result['guilds'];
+      
+      $result = add_zeros($result['guilds'], 5);
+      return $result;
     }
+  
+  function add_zeros($string, $number)
+  {
+    for ($i = 0; $i < $number; $i++) 
+    {
+      $string .= '0';
+    }
+    $string = substr($string, 0, $number);
+    return $string;
+  }
+}
+
+function append_string ($str1, $str2) {
+      
+    // Using Concatenation assignment
+    // operator (.=)
+    $str1 .=$str2;
+      
+    // Returning the result 
+    return $str1;
 }
 ?>
