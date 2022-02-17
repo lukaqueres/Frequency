a55925de41563d33bd1211464c7bb71448553b3f
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-17 09:07:17 +0100

    Update manageDB.php

--- web/manageDB.php
+++ web/manageDB.php
@@ -6,22 +6,18 @@ function pg_connection_string_from_database_url() {
   return "user=$user password=$pass sslmode=require host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
-// Here we establish the connection. Yes, that is all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-
 class DatabaseManager {
 
   private $result=0;
 
-  public function get_dbmembers()
+  public function get_dbmembers(pg_conn)
     {   
       $members = pg_query($pg_conn, "SELECT SUM(number_of_members) as total FROM servers_properties");
       $result = pg_fetch_assoc($members);
       return $result['total'];
     }
   
-  public function get_dbguilds()
+  public function get_dbguilds(pg_conn)
     {
       $result = pg_query($pg_conn, "SELECT COUNT(*) FROM servers_properties");
       $numberOfGuilds = pg_fetch_row($result);
