61206825ca5763b42fc0ea24c8bb3ddb7136481c
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-18 08:11:22 +0100

    Create login.php

---
+++ web/login.php
@@ -0,0 +1,57 @@
+logout') {
+  // This should logout you
+  logout($revokeURL, array(
+    'token' => session('access_token'),
+    'token_type_hint' => 'access_token',
+    'client_id' => OAUTH2_CLIENT_ID,
+    'client_secret' => OAUTH2_CLIENT_SECRET,
+  ));
+  unset($_SESSION['access_token']);
+  header('Location: ' . $_SERVER['PHP_SELF']);
+  die();
+}
+
+function apiRequest($url, $post=FALSE, $headers=array()) {
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+  $response = curl_exec($ch);
+
+
+  if($post)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+
+  $headers[] = 'Accept: application/json';
+
+  if(session('access_token'))
+    $headers[] = 'Authorization: Bearer ' . session('access_token');
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $response = curl_exec($ch);
+  return json_decode($response);
+}
+
+function logout($url, $data=array()) {
+    $ch = curl_init($url);
+    curl_setopt_array($ch, array(
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
+        CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded'),
+        CURLOPT_POSTFIELDS => http_build_query($data),
+    ));
+    $response = curl_exec($ch);
+    return json_decode($response);
+}
+
+function get($key, $default=NULL) {
+  return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+}
+
+function session($key, $default=NULL) {
+  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+}
+
+?>
