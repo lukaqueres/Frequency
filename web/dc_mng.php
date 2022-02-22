f7499d1744cecf1e76261f6d0a4b8761f718d010
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-22 20:12:16 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -32,7 +32,7 @@ require __DIR__ . "/config.php";
 <body>
 	<div id="header"> <span class="logo">Wild West Post Office</span>
 		<span class="menu">
-			<a class="log-in" href="https://web-plan-it.herokuapp.com/"> Main</a>
+			<a class="log-in" href="https://web-plan-it.herokuapp.com/">Main</a>
 			<?php
 			$auth_url = url($client_id, $redirect_url, $scopes);
 			if (isset($_SESSION['user'])) {
