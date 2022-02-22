60510427478ceabd3ec510404e8dce88a9dd3683
lukaqueres <57031937+lukaqueres@users.noreply.github.com>
2022-02-22 15:46:25 +0100

    Update login.php

--- web/includes/login.php
+++ web/includes/login.php
@@ -44,4 +44,4 @@ $_SESSION['guilds'] = get_guilds();
 $_SESSION['connections'] = get_connections();
 
 # Redirecting to home page once all data has been fetched
-redirect("../index.php");
+redirect("../dc_mng.php");
