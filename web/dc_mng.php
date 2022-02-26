b3413f177f16dc7c9c9d87129f0ea6e88d1e03a6
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-26 22:34:22 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -91,7 +91,6 @@ if (isset($_SESSION['user'])) {
 			<p> Profile Picture : <img src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']);
 			echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" /></p>
 		</div>
-			<br>
 		<!--<h2>User Response :</h2>
 		<div class="response-block">
 			<p><#?php echo json_encode($_SESSION['user']); ?></p>
