8485e3089f1534b3d4f42856570645bddfa9ef87
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-26 21:33:19 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -31,7 +31,25 @@ require __DIR__ . "/config.php";
 	<style>
 		.windowcontent { display: none; }</style>
 </head>
-
+<?php	
+if (isset($_SESSION['user'])) {
+	?>
+	<style type="text/css">
+		#error-info{
+			display:none;
+		}
+	</style>
+	<?php
+ }else {
+	?>
+	<style type="text/css">
+		#user-info{
+			display:none;
+		}
+	</style>
+	<?php
+}
+?>
 <body onload="load()">
 	<div id="aside_menu">
 		<span class="logo">Wild West Post Office</span>
