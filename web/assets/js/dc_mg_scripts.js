bd12a1720351a83dd5031aa02a4a9a20a31ec71e
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-26 21:10:16 +0100

    Update dc_mg_scripts.js

--- web/assets/js/dc_mg_scripts.js
+++ web/assets/js/dc_mg_scripts.js
@@ -1,7 +1,37 @@
-function changeuserwindowcontent($target, $button, $targetbutton) {
-  $button = document.getElementById($button)
-	$target = document.getElementById($target)
-	$targetbutton = document.getElementById($targetbutton)
+var windowIndex = 1;
+showWindow(windowIndex);
+
+function plusWindow(n) {
+  showWindow(windowIndex += n);
+}
+
+function currentWindow(n) {
+  showSlides(windowIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var windows = document.getElementsByClassName("windowcontent");
+  var btns = document.getElementsByClassName("windowContentBtn");
+  if (n > windows.length) {windowIndex = 1}    
+  if (n < 1) {windowIndex = windows.length}
+  for (i = 0; i < windows.length; i++) {
+      windows[i].style.display = "none";  
+  }
+  for (i = 0; i < btns.length; i++) {
+      btns[i].className = btns[i].className.replace(" active", "");
+  }
+  windows[windowIndex-1].style.display = "block";  
+  btns[windowIndex-1].className += " active";
+}
+
+function changeuserwindowcontent($button) {
+  $button_overview = document.getElementById('buttonoverview');
+  $button_servers = document.getElementById('buttonservers');
+  $
+  $button = document.getElementById($button);
+  $target = document.getElementById($target);
+  $targetbutton = document.getElementById($targetbutton);
   $test = 1;
   if ($target.classList.contains("show")) {
     $test = 1;
