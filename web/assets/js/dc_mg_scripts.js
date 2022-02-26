920312f4cc574d62b38197146245c931ad25a1c0
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-26 20:21:58 +0100

    Update dc_mg_scripts.js

--- web/assets/js/dc_mg_scripts.js
+++ web/assets/js/dc_mg_scripts.js
@@ -1,6 +1,6 @@
 function changewindowcontent($target, $button, $targetbutton) {
   $test = 1;
-  if $target.classList.contains("show") {
+  if ($target.classList.contains("show")) {
     $test = 1;
   } else {
     $button.classList.remove("active");
