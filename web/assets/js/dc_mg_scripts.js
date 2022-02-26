9f95dfd227b9745cbf6113fc57b5d1233d68c8e8
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-26 08:31:50 +0100

    Update dc_mg_scripts.js

--- web/assets/js/dc_mg_scripts.js
+++ web/assets/js/dc_mg_scripts.js
@@ -2,8 +2,10 @@ function changewindowcontent($target, $button, $targetbutton) {
   //if $target.classList.contains("show") {
     //$x = 1
   //} else {
+    $button.classList.remove("active");
+    $targetbutton.classList.add("active");
     $target.classList.toggle("show");
-    $button.classList.toggle("active");
-    $targetbutton.classList.toggle("active");
+    //$button.classList.toggle("active");
+    //$targetbutton.classList.toggle("active");
   //}
 }
