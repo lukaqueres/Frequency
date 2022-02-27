b7e39945b0d6fc149d9e17d07319a30abde1d0f0
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-27 17:50:25 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -58,7 +58,7 @@ if (isset($_SESSION['user'])) {
 		<div id="userPanel">
 			<img id="avatar" src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']);
 				echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" />
-			<h1><?php echo $_SESSION['username']?></h1>
+			<h1 style="float: left; padding: 25;"><?php echo $_SESSION['username']?></h1>
 		</div>
 		<?php
  		}
