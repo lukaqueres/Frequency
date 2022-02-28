855941c1627bf1569a7865bd03ad6f0921f39c68
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-28 19:01:35 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -104,7 +104,7 @@ if (isset($_SESSION['user'])) {
 		<div class="response-block">
 			<p><?php echo json_encode($_SESSION['user']); ?></p>
 			<?php
-	echo "Responce: <script> get_user_data(oid); </script>";
+	echo "Responce: <script> get_user_data(1111); </script>";
 		
             // handle a successful response
             //success : function(data) {
