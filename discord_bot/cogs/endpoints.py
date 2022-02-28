405a32814600610cfb549fbbdadba17b444203b6
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-02-28 17:20:57 +0100

    Update endpoints.py

--- discord_bot/cogs/endpoints.py
+++ discord_bot/cogs/endpoints.py
@@ -1,14 +1,22 @@
 import requests
 import json
 
-url = 'https://discord-bot-post-office.herokuapp.com/user_data' # <<<
-payload = {'name': '', 
-           'id': '', 
-           'create_date': '',
-           'has_nitro' : '', 
-           'is_in_servers' : ''
-          }
-response = requests.get(
-    url, params=payload,
-    headers={'Content-Type': 'application/json'} # Using JSON here for readability in the response
-)
+@app.route('/get_user_data', methods=['GET', 'POST'])
+def server():
+    if request.method == 'POST':
+        # Then get the data from the form
+        tag = request.form['tag']
+
+        # Get the username/password associated with this tag
+        user, password = tag_lookup(tag)
+
+        # Generate just a boring response
+        return 'The credentials for %s are %s and %s' % (tag, user, password) 
+        # Or you could have a custom template for displaying the info
+        # return render_template('asset_information.html',
+        #                        username=user, 
+        #                        password=password)
+
+    # Otherwise this was a normal GET request
+    else:   
+        return render_template('main.html')
