1daf2e718ecb85c695a113855a0dfd52cb6b6770
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-03-01 13:42:10 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -104,7 +104,7 @@ if (isset($_SESSION['user'])) {
 		<div class="response-block">
 			<p><?php echo json_encode($_SESSION['user']); ?></p>
 			<?php
-	echo $_SESSION;
+	echo $_SESSION['guilds_details'];
 		
             // handle a successful response
             //success : function(data) {
