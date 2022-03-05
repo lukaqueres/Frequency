8a7c5de9e56d2efac67ebc741bdf979140c4154c
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-03-05 15:19:33 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -73,8 +73,8 @@ if (isset($_SESSION['user'])) {
 		<!--</span>-->
 			</div>
 		<div style="margin-top: 100px;height: auto; width: auto;" class="padding_box">
-			<a href="https://web-plan-it.herokuapp.com/"><img class='imagebtn' src='images/serwers-icon-white.png'></a>
-			<a href="https://web-plan-it.herokuapp.com/"><img style="margin-top: 10px;" class='imagebtn' src='images/user-icon-white.png'></a>
+			<a href="https://web-plan-it.herokuapp.com/"><img class='imagebtn' src='images/user-icon-white.png'></a>
+			<a href="https://web-plan-it.herokuapp.com/"><img style="margin-top: 10px;" class='imagebtn' src='images/serwers-icon-white.png'></a>
 		</div>
 		<div style="bottom:0px; position: absolute;" class="padding_box">
 			<a href="https://web-plan-it.herokuapp.com/"> <img class='imagebtn' src='images/home-gear-white.png '></a>
