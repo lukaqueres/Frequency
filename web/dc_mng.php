ab10df28cf21a6d38ff398df9c3965988d8e905a
Lukas <57031937+lukaqueres@users.noreply.github.com>
2022-03-06 19:40:37 +0100

    Update dc_mng.php

--- web/dc_mng.php
+++ web/dc_mng.php
@@ -94,7 +94,7 @@ require __DIR__ . "/config.php";
 					<div class="window" id="guilds">Guilds
 					</div>
 				<?php } elseif ($_GET['window'] == 'user') { #WINDOW CONTENT ----------------------------------------------------------- USER ?>
-					<script type="text/javascript"> currentWindow(1); </script>
+					<script type="text/javascript"> load(); </script>
 					<div class="window" id="user-info">
 						<h1> User</h1>
 						<div class="linkheader">
@@ -156,7 +156,7 @@ require __DIR__ . "/config.php";
 	</body>
 	<script>
 		function load() {
-			var windowIndex = 1;
+			windowIndex = 1;
 			currentWindow(1);
 			//showWindow(windowIndex);
 		}
