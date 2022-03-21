334f9ee9928e9e2ab8b8b6fc786cf8d9504c3dec
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-21 13:01:08 +0100

    Update BOT_Wild_West_Post_Office.py

--- discord_bot/BOT_Wild_West_Post_Office.py
+++ discord_bot/BOT_Wild_West_Post_Office.py
@@ -11,7 +11,8 @@ from discord.utils import get
 from youtube_dl import *
 from discord.ext.commands import has_permissions, MissingPermissions, bot
 #from youtube_dl import YoutubeDL
-#from discord-py-slash-command import SlashCommands
+
+from discord_slash import cog_ext, SlashCommand
 
 from functions import get_prefix, get_time
 
@@ -141,7 +142,7 @@ bot.help_command = MyHelp()
 
 client = commands.Bot(command_prefix = get_prefix, intents=intents, help_command=MyHelp())
 bot = client
-#slash = SlashCommand(client)
+slash = SlashCommand(client)
 
 now = datetime.now() + timedelta(hours=2)
 today = date.today()
