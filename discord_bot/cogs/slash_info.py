d16146647256d2eeead12fab7eeb1c8490868b3f
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-21 11:45:38 +0100

    Create slash_info.py

---
+++ discord_bot/cogs/slash_info.py
@@ -0,0 +1,17 @@
+from discord import Embed
+from discord.ext.commands import Bot, Cog
+from discord_slash import cog_ext, SlashContext
+
+client = commands.Bot(command_prefix = get_prefix, Intents=intents)
+
+class Slash(Cog):
+    def __init__(self, client: Bot):
+        self.client = client
+
+    @cog_ext.cog_slash(name="test")
+    async def _test(self, ctx: SlashContext):
+        embed = Embed(title="Embed Test")
+        await ctx.send(embed=embed)
+
+def setup(client: client):
+    client.add_cog(Slash(client))
