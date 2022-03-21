8511870c3fecf254277da8414592d912522559e0
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-21 21:30:05 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -45,7 +45,7 @@ class Slash(Cog):
                                    description = "What commands do you want to check?",
                                    option_type = 3,
                                    required = False
-                                )]))
+                                )])
     async def _test(self, ctx: SlashContext):
         if not ctx.author.guild_permissions.manage_messages:
             return await ctx.send(">>> You can't use this!", hidden=True)
