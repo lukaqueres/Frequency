6c9150ddd6b59e490a070a7d4f13cf865949110f
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-22 13:02:50 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -36,7 +36,7 @@ class Slash(Cog):
                                 option_type = int,
                                 required = True
                                )])
-	async def _clear(ctx, number: int = None): 
+	async def _clear(ctx, number: int): 
 		if not ctx.author.guild_permissions.manage_messages:
 			return await ctx.send(">>> You can't use this!", hidden=True)
 		if number == None:
