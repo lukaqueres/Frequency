c1035bc6b17529ed2389751f757a6fa52f2b2f1c
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-22 20:08:48 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -60,8 +60,8 @@ class Slash(Cog):
 	async def _clear(self, ctx: SlashContext, ammount: int = 100, user = None, days = 21, role = None): 
 		if ammount <= 0 and ammount > 100:
 			return await ctx.send(">>> Invalid number given. Number must fit between 1 and 100", hidden=True)
-        if days <= 0 and days > 21:
-            return await ctx.send(">>> Invalid days count given. Messages to delete can be max 21 days old", hidden=True)
+		if days <= 0 and days > 21:
+			return await ctx.send(">>> Invalid days count given. Messages to delete can be max 21 days old", hidden=True)
 		await ctx.send(f"No {ammount or 0 } by user {user or 'NO'} \n CTX: {ctx}", hidden=True)
         
 	@cog_ext.cog_slash(name="help", 
