32e5c260e722717c158a660bca80c722d25d4633
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-23 15:56:57 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -155,11 +155,7 @@ class Slash(Cog):
 		messages_count = []
 		guild_channels = ctx.guild.text_channels
 		after_date = datetime.utcnow()-timedelta(days=7)
-		for channel in guild_channels:
-			total_messages_count = 0
-			messages_count[channel.name] = 0
-			async for message in channel.history(limit=500, after=after_date):
-				messages_conut[channel.name] += 1
+		
 		rolelist = [r.name for r in user.roles if r != ctx.guild.default_role]
 		roles = " | ".join(reversed(rolelist))
 		account_created = user.created_at.strftime("%d/%m/%Y %H:%M:%S")
@@ -176,11 +172,12 @@ class Slash(Cog):
 		embed.add_field( name="All roles:", value=roles, inline=False),
 		embed.add_field(name = chr(173), value = f"**Status**: {str(user.status).title()}\n**Activity**: {str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}\n**Bot**: {'NO' if not user.bot else 'YES'}", inline=True),
 		embed.add_field( name= chr(173), value=f"**Top role**: {user.top_role.name}\n**Number of roles**: {len(rolelist)}\n**Nitro**: { 'Yes' if bool(user.premium_since) else 'No'}", inline=True),
-		for channel in messages_count:
-			if channel = 0:
-				continue
-			else:
-				embed.add_field( name= f"Messages count in channel: {channel}", value=messages_count[channel], inline=True),
+		for channel in guild_channels:
+			total_messages_count = 0
+			messages_count = 0
+			async for message in channel.history(limit=500, after=after_date):
+				messages_conut[channel.name] += 1
+			embed.add_field( name= f"Messages count in channel: {channel.name}", value=messages_count, inline=True),
 		embed.set_footer(text="Provided by Wild West Post Office")
 		await ctx.send( embed = embed , hidden = True)
 		
