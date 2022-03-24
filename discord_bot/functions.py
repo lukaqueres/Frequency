4db47fea30fc024133091929a8149095f0e24d90
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-24 19:12:06 +0100

    Update functions.py

--- discord_bot/functions.py
+++ discord_bot/functions.py
@@ -118,21 +118,25 @@ def check_database(guilds):
 		guilds_id.append(guild.id)
 		print('Database Check: Guild {} check.'.format( guild ))
 		cur.execute(f"""
+		   DO $$
+		   BEGIN 
 		   IF NOT EXISTS ( SELECT 1 FROM servers_properties WHERE guild_id = {guild.id} ) 
-		   BEGIN
+		   THEN
                    	INSERT INTO SERVERS_PROPERTIES ( GUILD_ID, GUILD_NAME, DATE_OF_JOIN, GUILD_PREFIX, NUMBER_OF_USERS, message_check_feature, ECONOMY, MUSIC, UPDATES, NUMBER_OF_MEMBERS, GUILD_LANGUAGE) 
                    	VALUES ('{guild.id}', '{guild.name}', '{date_of_join}', '{default_prefix}', '{guild.member_count}', '{"NO"}', '{"NO"}', '{"YES"}', '{"NO"}', '{members_count}', '{default_language}');
-                   END
+                   END IF
 		   IF EXISTS ( SELECT 1 FROM servers_data WHERE guild_id = {guild.id} ) 
-                   BEGIN
+                   THEN
 		   	INSERT INTO servers_data ( guild_id, guild_name, music_volume) WHERE guild_id = {guild.id}
                    	VALUES ('{guild.id}', '{guild.name}', '{50}');
-		   END
+		   END IF
 		   IF EXISTS ( SELECT 1 FROM servers_msg_process WHERE guild_id = {guild.id} ) 
-		   BEGIN
+		   THEN
 		   	INSERT INTO servers_msg_process ( guild_id, guild_name ) 
 		   	VALUES ( '{guild.id}', '{guild.name}' );
+		   END IF
 		   END
+		   $$
 		   """);
 		con.commit()
 		print(f"Succesful database actualization for guild {guild.name}")
