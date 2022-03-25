dae239fffa0b27a9ee3e717ca035c57e032fbaea
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-25 21:24:15 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -318,18 +318,18 @@ class Slash(Cog):
 		#await member.ban(reason=reason, delete-message-days=timerange)
 		try:
 			await ctx.guild.ban(member, reason=reason)
-		except MissingPermissions:
-			embed = Embed(title="User don't banned",
+		except discord.errors.Forbidden:
+			embed = Embed(title="Failed to ban user",
 			colour = user.colour,
 			#timestamp=get_time()
 			)
 			embed.set_thumbnail(url=user.avatar_url)
 			embed.add_field( name=chr(173), value=f"**User**: {str(user)}\n**User ID**: {user.id}", inline=True),
 			embed.add_field( name=chr(173), value=f"**Command invoked by**: {str(ctx.author.name)}\n**User ID**: {ctx.author.id}", inline=True),
-			embed.add_field( name="Command failed", value=f"Because of missing bot permission", inline=False),
+			embed.add_field( name="Command failed", value=f"Because of missing bot permissions", inline=False),
 			return await ctx.send(embed=embed)
 		except:
-			embed = Embed(title="User don't banned",
+			embed = Embed(title="Failed to ban user",
 			colour = user.colour,
 			#timestamp=get_time()
 			)
