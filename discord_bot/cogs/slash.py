37d6c18c996bc45c79b7951e750d35a281b217c3
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-25 15:25:07 +0100

    Update slash.py

--- discord_bot/cogs/slash.py
+++ discord_bot/cogs/slash.py
@@ -193,7 +193,7 @@ class Slash(Cog):
 			embed.add_field( name=chr(173), value=f"**User**: {str(user)}\n**User ID**: {user.id}", inline=True),
 			embed.add_field( name=chr(173), value=f"**Created**: {account_created}, **{cdays}** days ago \n**Joined**: {guild_join}, **{jdays}** days ago", inline=True),
 			#embed.add_field(name = chr(173), value = chr(173), inline=False)
-			embed.add_field( name="All roles:", value=roles , inline=False), #if roles else "No roles"
+			embed.add_field( name="All roles:", value=roles if roles else "No roles", inline=False), #if roles else "No roles"
 			embed.add_field(name = chr(173), value = f"**Status**: {str(user.status).title()}\n**Activity**: {str(user.activity.type).split('.')[-1].title() if user.activity else 'N/A'} {user.activity.name if user.activity else ''}\n**Bot**: {'NO' if not user.bot else 'YES'}", inline=True),
 			embed.add_field( name= chr(173), value=f"**Top role**: {user.top_role.name}\n**Number of roles**: {len(rolelist)}\n**Nitro**: { 'Yes' if bool(user.premium_since) else 'No'}", inline=True),
 			guild_channels = ctx.guild.text_channels
