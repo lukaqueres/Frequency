031d431806c2ce3af57c191a456fe976fdffeed7
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-03-28 17:56:22 +0200

    Update slash_music.py

--- discord_bot/cogs/slash_music.py
+++ discord_bot/cogs/slash_music.py
@@ -556,13 +556,14 @@ class Slash_music(Cog):
 				return i.message.id == music_console_msg.id
 			state = True
 			while state:
-				interaction, button = await self.client.wait_for("button_click", check=check_button) # lambda i: i.component.custom_id == "queue"
+				event = await self.client.wait_for("button_click", check=check_button) # , button
+				button = event.component
 				#message_reference = await ctx.channel.fetch_message(music_console_msg)
 				embed = discord.Embed(title='You pressed an Button',
 					      description=f'You pressed a {button.emoji} button.',
 					      color=discord.Color.random())
 				#await ctx.send(embed=embed, delete_after = 60) #, reference = music_console_msg
-				await interaction.respond.send(embed = embed) #, delete_after = 5
+				await event.respond(embed = embed) #, delete_after = 5
 	
 			
 			#await client.wait_for("button_click", check = lambda i: i.component.emoi == '🎶')
