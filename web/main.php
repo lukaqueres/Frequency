72a20c4fc393ef18542837944c7b0b57cff4d667
lukaqueres <lkocieln@gmail.com>
2022-03-31 18:36:23 +0200

    lets gooooo

--- web/main.php
+++ web/main.php
@@ -27,7 +27,7 @@
             <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Main</a>
             <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Commands &amp; Modules</a>
             <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Updates &amp; Changelog</a>
-            <div>
+            <div class="no_margin">
                 <?php
                 if (isset($_SESSION['user']))
                 {
@@ -48,21 +48,42 @@
 
         <div class="grid_container">
             <div class="header">
-                Header
+                <div id="header_panel" class="vertical_center flex_container">
+                    <h1 class="no_margin"> <?php echo $bot_name; ?> </h1>
+                    <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Main</a>
+                    <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Commands &amp; Modules</a>
+                    <a class="clearlink" href=<?php echo $bot_invite_link; ?> >Updates &amp; Changelog</a>
+                    <div class="no_margin">
+                        <?php
+                        if (isset($_SESSION['user']))
+                        {
+                        echo "<a id='headerbtn' href='./dc_mng'>
+                            Manage
+                        </a>";
+                        }
+                        else
+                        {
+                        $auth_url = url($client_id, $redirect_url, $scopes);
+                        echo "<a id='headerbtn' href='$auth_url'>
+                            Authorize
+                        </a>";
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="left">
+                    Left
+                </div>
+                <div class="main">
+                    Main
+                </div>
+                <div class="right">
+                    Right
+                </div>
+                <div class="footer">
+                    Foooooooooter
+                </div>
             </div>
-            <div class="left">
-                Left
-            </div>
-            <div class="main">
-                Main
-            </div>
-            <div class="right">
-                Right
-            </div>
-            <div class="footer">
-                Foooooooooter
-            </div>
-        </div>
 
     </body>
 </html> 
