a9c553751bd578d92ad8fed61f738cf6bb5d233e
lukaqueres <lkocieln@gmail.com>
2022-04-01 14:45:09 +0200

    Update manage.php

--- web/manage.php
+++ web/manage.php
@@ -212,12 +212,12 @@ require __DIR__ . "/config.php";
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.add("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('#servers_search')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
