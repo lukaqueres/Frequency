fdb9ead9cf607c69bdb2812a4fb86aa1350a71ac
lukaqueres <lkocieln@gmail.com>
2022-04-01 15:34:41 +0200

    summary

--- web/manage.php
+++ web/manage.php
@@ -78,8 +78,10 @@ require __DIR__ . "/config.php";
 								</table>
 						</div>
 					</div>
-					<img id="avatar" src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']); echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" />
-					<span id="avatar_usename"><?php echo $_SESSION['username']?></span>
+					<div>
+						<img id="avatar" src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']); echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" />
+						<span id="avatar_usename"><?php echo $_SESSION['username']?></span>
+					</div>
 				<?php } else 
 					{
 						$auth_url = url($client_id, $redirect_url, $scopes);
