dc59ecc162e4b778c7fa509b5699569c76d30f57
lukaqueres <lkocieln@gmail.com>
2022-04-02 09:58:00 +0200

    Work on panel left of main window in manage page

--- web/manage.php
+++ web/manage.php
@@ -100,12 +100,13 @@ require __DIR__ . "/config.php";
 					<div class="header_div_box">
 						<h1 class="no_margin">
 							<?php
-								$extention = is_animated($_SESSION['user_avatar']);
+								$extention = is_animated($_SESSION['guilds'][$i]['icon']);
 								if (isset($_GET['guild_id'])) {
 									for ($i = 0; $i < sizeof($_SESSION['guilds']); $i++) {
 										if ($_SESSION['guilds'][$i]['id'] == $_GET['guild_id']) {
-											echo '<img id="avatar" src="https://cdn.discordapp.com/avatars/' . $_SESSION['user_id'] . '/' . $_SESSION['user_avatar'] . $extention . '"/>';
+											echo '<img id="avatar" src="https://cdn.discordapp.com/avatars/' . $_SESSION['guilds'][$i]['id'] . '/' . $_SESSION['guilds'][$i]['icon'] . $extention . '"/>';
 											echo $_SESSION['guilds'][$i]['name'];
+											break;
 										}
 									}
 								} elseif (isset($_SESSION['username'])) {
