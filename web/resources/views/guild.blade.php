5fbcf21a0210938efa21c6c3d80cbaebc5f03e4d
lukaqueres <lkocieln@gmail.com>
2022-04-08 16:48:15 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -1,1 +1,20 @@
-<?php      if (isset($id)) {         echo '<b>Passed: ' . $id . ' </b>';         //echo get_guild_tags(['guild','user']);     } else {         echo 'no $id';     }     if (isset($view)) {         echo '<b>Passed: ' . $view . ' </b>';     } else {         echo 'Pusta zmienna';     }     if (isset($id) and($id == 0)) {         return redirect()->route('manage');     } ?> Guild {{ Session::get('name')}}  Yay 
+<?php
+
+    if (isset($id)) {
+        echo '<b>Passed: ' . $id . ' </b>';
+        //echo get_guild_tags(['guild','user']);
+    } else {
+        echo 'no $id';
+    }
+    if (isset($view)) {
+        echo '<b>Passed: ' . $view . ' </b>';
+    } else {
+        echo 'Pusta zmienna';
+    }
+    if (isset($id) and($id == 0)) {
+        return redirect()->route('manage');
+    }
+?>
+Guild {{ Session::get('name')}}
+
+Yay
