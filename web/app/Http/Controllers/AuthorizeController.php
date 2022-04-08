ec7514be7f49226e5f1093774bb2ca13b4630a63
lukaqueres <lkocieln@gmail.com>
2022-04-08 16:15:51 +0200

    Update AuthorizeController.php

--- web/app/Http/Controllers/AuthorizeController.php
+++ web/app/Http/Controllers/AuthorizeController.php
@@ -1,4 +1,4 @@
-<?php  namespace Jakyeru\Larascord\Http\Controllers;  use Illuminate\Http\Request; use Illuminate\Support\Facades\Auth; use Illuminate\Support\Facades\Http; use App\Models\User; use App\Providers\RouteServiceProvider;  class DiscordController extends Controller { 
+<?php  namespace Jakyeru\Larascord\Http\Controllers;  use Illuminate\Http\Request; use Illuminate\Support\Facades\Auth; use Illuminate\Support\Facades\Http; use App\Models\User; use App\Providers\RouteServiceProvider;  class AuthorizeController extends Controller { 
     public function discord_auth(Request $request)
     {
         Session::put('name', 'John Doe');
