2a1815ee0cd9c222e6c0774f1f59306073962596
lukaqueres <lkocieln@gmail.com>
2022-04-08 16:44:17 +0200

    Update AuthorizeController.php

--- web/app/Http/Controllers/AuthorizeController.php
+++ web/app/Http/Controllers/AuthorizeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AuthorizeController extends Controller { 
+class AuthorizeController extends Controller
+{
+
     public function discord_auth(Request $request)
     {
         Session::put('name', 'John Doe');
-        return view('guild'); 
+        return view('guild');
     }
 
 }
