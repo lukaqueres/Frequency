849f9e5c260229b01182d467e862ba296038286c
lukaqueres <lkocieln@gmail.com>
2022-04-08 16:24:34 +0200

    Fix helpers.php error by commenting everything

--- web/app/Http/Controllers/AuthorizeController.php
+++ web/app/Http/Controllers/AuthorizeController.php
@@ -1,4 +1,6 @@
-<?php  namespace Jakyeru\Larascord\Http\Controllers;  use Illuminate\Http\Request; use Illuminate\Support\Facades\Auth; use Illuminate\Support\Facades\Http; use App\Models\User; use App\Providers\RouteServiceProvider;  class AuthorizeController extends Controller { 
+<?php  namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;  class AuthorizeController extends Controller { 
     public function discord_auth(Request $request)
     {
         Session::put('name', 'John Doe');
