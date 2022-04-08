a2248a7c9e78fa49bfb3a7b31a5cf5b14789e918
lukaqueres <lkocieln@gmail.com>
2022-04-08 18:48:16 +0200

    Update auth.php

--- web/config/auth.php
+++ web/config/auth.php
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'database',
             'model' => App\Models\User::class,
         ],
 
