d44054d9dd75b9de83ed2d98d97a2ee53b6816ae
lukaqueres <lkocieln@gmail.com>
2022-04-09 09:35:51 +0200

    Create 2022_09_04_000010_create_sessions_table.php

---
+++ web/database/migrations/2022_09_04_000010_create_sessions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('ip_address')->nullable();
+            $table->string('username');
+            $table->string('discriminator');
+            //$table->string('email')->unique();
+            //$table->timestamp('email_verified_at')->nullable();
+            //$table->string('password');
+            $table->string('avatar')->nullable();
+            $table->array('payload')->nullable();
+            //$table->rememberToken();
+            $table->string('refresh_token')->nullable();
+            $table=>integer('last_activity')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
