7d23ae9ba769ca827354c0532191b9d2b7541775
lukaqueres <lkocieln@gmail.com>
2022-04-10 21:30:40 +0200

    Update MainController.php

--- web/app/Http/Controllers/MainController.php
+++ web/app/Http/Controllers/MainController.php
@@ -3,7 +3,7 @@
 namespace app\Http\Controller\Main;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\App\Http\Controllers\Controller;
 
 class MainController extends Controller
 {
