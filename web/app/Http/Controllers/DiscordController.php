c1f6d4b01a69a7e5c6c559e4965d2aabe8ebf75a
lukaqueres <lkocieln@gmail.com>
2022-04-10 21:27:57 +0200

    Update DiscordController.php

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Discord;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\Controller;
 
 class DiscordController extends Controller
 {
