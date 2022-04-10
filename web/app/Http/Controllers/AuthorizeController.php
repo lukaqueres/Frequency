f5ede1d8b69b6e5b2677af556932e25bd978872d
lukaqueres <lkocieln@gmail.com>
2022-04-10 12:01:02 +0200

    Update AuthorizeController.php

--- web/app/Http/Controllers/AuthorizeController.php
+++ web/app/Http/Controllers/AuthorizeController.php
@@ -14,7 +14,7 @@ class AuthorizeController extends Controller
         
         Session::put('name', 'John Doe');
         session()->put('username', 'John_Doe');
-        Session::save()
+        Session::save();
         return view('guild');
     }
 }
