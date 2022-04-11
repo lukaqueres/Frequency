5ad8acc20e648401c51a3c002f2139a3cfbc1335
lukaqueres <lkocieln@gmail.com>
2022-04-11 18:59:26 +0200

    ?Isuue 4

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Controller;
