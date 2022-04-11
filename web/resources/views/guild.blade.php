4cfdf00ef8bfb69077ab9ab900881c8ff5a2a67a
lukaqueres <lkocieln@gmail.com>
2022-04-11 19:26:31 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -1,3 +1,3 @@
 Guild</br>
 NAME:{{Session::get('name')}}</br>
-USER_DATA: <?php echo json_encode(Session::get('user_data'); ?></br>
+USER_DATA: <?php echo json_encode(Session::get('user_data')); ?></br>
