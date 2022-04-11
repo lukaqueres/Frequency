f6b49c0b0891c3d70e7a573559246c7660be1f83
lukaqueres <lkocieln@gmail.com>
2022-04-11 16:40:13 +0200

    Update main.blade.php

--- web/resources/views/main.blade.php
+++ web/resources/views/main.blade.php
@@ -1,3 +1,4 @@
 Main</br>
 NAME:{{Session::get('name')}}</br>
 USERNAME: {{Session::get('username')}}</br>
+https://discord.com/api/oauth2/authorize?client_id=875271995644842004&redirect_uri=https%3A%2F%2Fweb-plan-it.herokuapp.com%2Fdiscord%2Fauthorize&response_type=code&scope=identify%20email%20guilds%20guilds.members.read
