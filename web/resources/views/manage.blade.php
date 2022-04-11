2300e2aa2c4b20391ed02075a686e1cd2f3ff938
lukaqueres <lkocieln@gmail.com>
2022-04-11 21:52:48 +0200

    Update manage.blade.php

--- web/resources/views/manage.blade.php
+++ web/resources/views/manage.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title> Wild West Post Office | Manage</title>
-    <link rel="stylesheet" type="text/css" href="App/public/assets/css/foundation.css" />
-    <script type="text/javascript" src="assets/js/scripts.js"></script>
-    <meta name="description" content="Main site featuring Discord multi-task bot Wild West Post Office!" />
-    <meta name="keywords" content="discord, bot" />
-    <meta name="author" content="Lukas" />
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-Manage</br>
-USER_DATA: <?php echo json_encode(Session::get('user_data')); ?></br>
-GUILDS_DATA: <?php echo json_encode(Session::get('guilds_data')); ?></br>
+    <head>
+        <title> Wild West Post Office | Manage</title>
+	    <link rel="stylesheet" type="text/css" href="/app/public/assets/css/foundation.css" />
+	    <script type="text/javascript" src="assets/js/scripts.js"></script>
+	    <meta name="description" content="Main site featuring Discord multi-task bot Wild West Post Office!" />
+	    <meta name="keywords" content="discord, bot" />
+	    <meta name="author" content="Lukas" />
+	    <meta charset="UTF-8" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        Manage</br>
+        USER_DATA: <?php echo json_encode(Session::get('user_data')); ?></br>
+        GUILDS_DATA: <?php echo json_encode(Session::get('guilds_data')); ?></br>
+    </body>
+</html>
