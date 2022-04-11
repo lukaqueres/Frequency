6f1d36640988a6a73951157f82c84c4da6c550ce
lukaqueres <lkocieln@gmail.com>
2022-04-11 21:53:17 +0200

    Update main.blade.php

--- web/resources/views/main.blade.php
+++ web/resources/views/main.blade.php
@@ -1,2 +1,5 @@
 Main</br>
 <a href='https://discord.com/api/oauth2/authorize?client_id=875271995644842004&redirect_uri=https%3A%2F%2Fweb-plan-it.herokuapp.com%2Fdiscord%2Fauthorize&response_type=code&scope=identify%20email%20guilds%20guilds.members.read'> AUTHORIZE </a>
+<?php if (Session::exists('status')) {
+    echo 'status: ' . json_encode(Session::get('status'));
+}
