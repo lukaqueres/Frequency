df07a268dac0297172c6b0fe9ff42e4318c491fe
lukaqueres <lkocieln@gmail.com>
2022-04-11 16:38:41 +0200

    Update DiscordController.php

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -11,12 +11,15 @@ class DiscordController extends Controller
 
 	public function authorizeMe(Request $request)
 	{
-		//$session = SessionModel::all();
-		
-		Session::put('name', 'John Doe');
-		Session::put('username', 'lukaqueres');
-		Session::save();
-        var_dump($request);
+        if ($request->isMethod('post')) {
+            $value = $request->header("access_token");
+            echo $valuel;
+            Session::put('name', 'John Doe');
+		    Session::put('username', 'lukaqueres');
+		    Session::save();
+        }
+
+        //var_dump($request);
 		return view('guild'); 
 	}
 }
