313e52e7be93fe6bd9db95586c7df3902535c53c
lukaqueres <lkocieln@gmail.com>
2022-04-12 13:20:39 +0200

    Update DiscordController.php

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -34,7 +34,7 @@ class DiscordController extends Controller
         return json_decode($response->body());
     }
 
-    private function getUser(string $access_token): object // Return Discord's user object. It contain data such as nick, avatar, if verified or e-mail
+    private function getUser(string $access_token): array // Return Discord's user array. It contain data such as nick, avatar, if verified or e-mail
     {
         $response = Http::withToken($access_token)->get($this->apiURLBase);
 
