75c50a5ba73d2fc936b0df9d725956f3a553ea06
lukaqueres <lkocieln@gmail.com>
2022-04-12 13:38:41 +0200

    Update DiscordController.php

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -126,7 +126,7 @@ class DiscordController extends Controller
         $data['user'] = $user;
 
         foreach($guilds as $guild) {
-            var_dump($guild['permissions'])
+            var_dump($guild['permissions']);
             $guild['permissions_names'] = get_permissions($guild['permissions']);
             $guild['p_tag'] = get_user_permissions_tag($guild['permissions_names']);
             $guild['tags'] = get_guild_tags($guild);
