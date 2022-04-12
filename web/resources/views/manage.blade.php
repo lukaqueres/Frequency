3d26fa776ed4ef9a011bc8a20e970f7f4143daa1
lukaqueres <lkocieln@gmail.com>
2022-04-12 17:40:31 +0200

    Start making css and manage site

--- web/resources/views/manage.blade.php
+++ web/resources/views/manage.blade.php
@@ -11,7 +11,11 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        Manage</br>
-        DATA: <?php echo json_encode(Session::get('data')); ?></br>
+        <div id="container">
+        <div id="header">
+            HEADER
+        </div>
+            DATA: <?php echo json_encode(Session::get('data')); ?></br>
+        </div>
     </body>
 </html>
