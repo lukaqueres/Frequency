8fd8b81d6b32af756080801057e8e0c645aa305f
lukaqueres <lkocieln@gmail.com>
2022-04-12 14:36:29 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -1,8 +1,10 @@
 Guild</br>
 NAME:{{Session::get('name')}}</br>
 GUILD_DATA: <?php
-$data = Session::get('data')
+$data = Session::get('data');
 if (Arr::exists($data[$id]))  {
     echo json_encode($data[$id]);
+} else {
+    echo 'No guild found';
 }
 ?></br>
