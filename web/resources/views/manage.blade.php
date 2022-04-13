41e952d3aa313f3998c59be3f1c6e499c176bc45
lukaqueres <lkocieln@gmail.com>
2022-04-13 14:57:11 +0200

    Add flexbox

--- web/resources/views/manage.blade.php
+++ web/resources/views/manage.blade.php
@@ -20,7 +20,7 @@
                     $guilds = Session::get('data')['guilds']['snippets'];
                     foreach($guilds as $guild) {
                         echo '
-                        <div class="inline-margin zoom">
+                        <div class="flex-box inline-margin zoom">
                             <img class="icon inline-margin" src="' . get_icon($guild) .'"/>
                             <h1>' . $guild['name'] .' </h1>
                         </div>';
