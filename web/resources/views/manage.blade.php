39f4852ddcda8185dfb2750f960d0456a2662e2d
lukaqueres <lkocieln@gmail.com>
2022-04-13 18:33:29 +0200

    Update manage.blade.php

--- web/resources/views/manage.blade.php
+++ web/resources/views/manage.blade.php
@@ -23,25 +23,20 @@
                 <h1 class="inline-margin">HEADER</h1>
             </div>
             <div class="left">
-                <div class="grid-container">
-                    <?php
-                        $flow = '<div class="grid-container">';
-                        $count = 0;
+                <?php
+                    $flow = '<div class="grid-container"><div class="grid-x grid-margin-x small-up-2 medium-up-3">';
+                    $count = 0;
                         foreach($snippets as $guild) {
-                            if ($count == 0) { $flow .= '<div class="grid-x grid-margin-x small-up-2 medium-up-3">'; }
                             $flow .= '<div class="cell"><div class="card">
-                                        
                                         <div class="card-section">
                                         <img class="columns align-self-middle shrink icon inline-margin" src="' . get_icon($guild) .'"/>
                                         <h4>' . $guild['name'] . '</h4>
                                         <p> TAGS </p>
-                                    </div></div></div>';
-                            //if ($count == 3) { $flow .= '</div>'; $count = 0; continue;}
+                                        </div></div></div>';
                             $count = $count + 1;
                         }
-                        echo $flow . '</div>';
-                    ?>
-                    </div>
+                    echo $flow . '</div>';
+                ?>
             </div>
             <div class="main">
                 <h5>DATA: <?php echo json_encode(Session::get('data'), JSON_PRETTY_PRINT); ?></br></h5>
