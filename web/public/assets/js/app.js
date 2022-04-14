02129f795079c36c79f9a9a3bae77615e57d3d20
lukaqueres <lkocieln@gmail.com>
2022-04-14 16:26:45 +0200

    Update app.js

--- web/public/assets/js/app.js
+++ web/public/assets/js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation()
+
+function search_guilds() {
+    var input, filter, tabel, tr, a, i, txtValue;
+    input = document.getElementById("app-servers-search");
+    filter = input.value.toUpperCase();
+    tabel = document.getElementById("app-servers");
+    tr = tabel.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        //a = tr[i].getElementsByTagName("a")[0];
+        a = tr[i];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
