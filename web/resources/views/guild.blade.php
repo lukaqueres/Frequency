7c1165c31eb28afa548e819204d055557c939b0c
lukaqueres <lkocieln@gmail.com>
2022-04-15 14:31:19 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -32,7 +32,7 @@
                     <div style="background-color: black; width: 100%; position: sticky; top: 0;"><input type="text" id="app-servers-search" onkeyup="search_guilds();" placeholder= <?php echo '"' . $thisGuild->name . '" '; ?> title="Filter by name or id"></div>
                         <table style="border-collapse: collapse; border-spacing: 0; width: 100%;" id="app-servers">
                         <tr class="app-flex-container" style="padding-block: 10px;">
-                        	<?php echo '<td class="app-flex-container" style="width: 100%; padding: 5px;" ><a class="app-flex-container app-no-margin app-link-tag" href="#">
+                        	<?php echo '<td class="app-flex-container" style="width: 100%; padding: 5px;" ><a class="app-flex-container app-no-margin app-link-tag app-zoom" href="#">
 								<img class="app-icon" src="' . get_avatar($user) . '"/>
 								<h3 class="app-no-margin">' . $user->username . '</h3></a>
 								</td>
@@ -41,7 +41,7 @@
                             <?php
                                 foreach($snippets as $guild) {
 									echo '<tr id="search_dropdown_' . $guild['id'] .'_" class="app-flex-container" >
-										<td class="app-flex-container" style="width: 100%; padding: 5px;" ><a class="app-flex-container app-no-margin app-link-tag" href="/manage/guild/' . $guild['id'] . '">
+										<td class="app-flex-container" style="width: 100%; padding: 5px;" ><a class="app-flex-container app-no-margin app-link-tag app-zoom" href="/manage/guild/' . $guild['id'] . '">
 											<img class="app-icon" src="' . get_icon($guild) . '"/>
 											<h3 class="app-no-margin">' . $guild['name'] . '</h3></a>
 										</td>
