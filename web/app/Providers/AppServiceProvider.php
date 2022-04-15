5d772d61f30a1b026abafeb985f057196aa65bc6
lukaqueres <lkocieln@gmail.com>
2022-04-15 16:12:10 +0200

    Update AppServiceProvider.php

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
-
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceScheme('https');
         // any other codes here, does not matter.
     }
 }
