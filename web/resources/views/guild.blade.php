e0b4de48b3364b9f71475f4a3139cb641ebc0e67
lukaqueres <lkocieln@gmail.com>
2022-04-22 17:55:18 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -33,7 +33,7 @@
                         <table style="border-collapse: collapse; border-spacing: 0; width: 100%;" id="app-servers">
                         <tr class="app-flex-container" style="padding-block: 10px;">
                         	<?php echo '<td class="app-flex-container" style="width: 100%; border-bottom: 1px #18181E solid;; padding: 5px;" ><a class="app-flex-container app-no-margin app-link-tag app-zoom" href="/manage/user">
-								<img class="app-icon" src="' . get_avatar($user) . '"/>
+								<img class="app-icon" src="' . get_avatar($user) . '"/> 
 								<h3 class="app-no-margin">' . $user->username . '</h3></a>
 								</td>
 								<td class="app-no-display">' . $user->id . '</td>'; ?>
