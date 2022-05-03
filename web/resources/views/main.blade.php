886bf298b0007a6060c9b6c0a2b38d8cc0b33a73
lukaqueres <lkocieln@gmail.com>
2022-05-03 09:54:16 +0200

    Make it better

--- web/resources/views/main.blade.php
+++ web/resources/views/main.blade.php
@@ -11,7 +11,7 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        Main PUSH COMMIT</br>
+        Main</br>
         <a href='https://discord.com/api/oauth2/authorize?client_id=875271995644842004&redirect_uri=https%3A%2F%2Fweb-plan-it.herokuapp.com%2Fdiscord%2Fauthorize&response_type=code&scope=identify%20email%20guilds%20guilds.members.read'> AUTHORIZE </a>
         <?php if (Session::exists('status')) {
             echo 'status: ' . json_encode(Session::get('status'));
