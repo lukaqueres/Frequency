b9f797133785203c246938abfb2f4fc7a52be971
lukaqueres <lkocieln@gmail.com>
2022-05-08 19:04:05 +0200

    Add filters to search guilds pop-up

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -29,7 +29,7 @@
                     <input type="text" id="filter-guilds" onkeyup="filter_guilds();" placeholder= <?php echo '"' . $thisGuild->name . '" '; ?> title="Filter by name or id">
                     <div>
                     <?php
-                        foreach($guilds['tags'] as $tag) {
+                        foreach($thisGuild['tags'] as $tag) {
 						    echo '<button class="tag">' . $tag .'</button>';
 						}
                     ?>
