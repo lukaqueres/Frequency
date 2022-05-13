3da1e6bdf0369178dcd2cc044f27e73990b8d5c7
lukaqueres <lkocieln@gmail.com>
2022-05-13 21:21:44 +0200

    Cd of working on site:

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -59,12 +59,12 @@
             </div>
             <div class="page-header flex">
                 <a class="small text" href="/manage">< Return</a>
-                <button class="center" onclick="togglePopUp('guildPop-up')">
+                <button class="top-btn top center" onclick="togglePopUp('guildPop-up')">
                     <?php
                         echo $thisGuild->name;
                     ?>
                 </button>
-                <img class="icon right" src= <?php echo '"' . get_avatar($user) . ' " >' ?>
+                <img class="icon right" src= <?php echo '"' . get_avatar($user) . ' " >'; ?>
             </div>
             <div class="page-aside">
                 <button onclick="togglePopUp('guildPop-up')">
