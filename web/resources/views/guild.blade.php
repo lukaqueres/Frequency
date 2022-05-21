1d944351775105fb69b4cee8546b9d5dea27f8ea
lukaqueres <lkocieln@gmail.com>
2022-05-21 11:51:42 +0200

    Guilds style

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -60,7 +60,7 @@
             <div class="page-header flex">
                 <a class="small text" href="/manage">< Return</a>
                 <button type="text" id="head-button" onclick="togglePopUp('guildPop-up')" title="Search for guilds by name or id"><?php echo 'Search for guilds | ' . $thisGuild->name; ?></button>
-                <button class="top-btn top center" onclick="togglePopUp('guildPop-up')">Choose guild</button>
+                <!--<button class="top-btn top center" onclick="togglePopUp('guildPop-up')">Choose guild</button>-->
                 <img class="icon right" src= <?php echo '"' . get_avatar($user) . ' " >'; ?>
             </div>
             <div class="page-aside">
