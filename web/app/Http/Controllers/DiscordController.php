5d16d700d65522a30dee3b0f3501b91b959d0b4f
lukaqueres <lkocieln@gmail.com>
2022-05-28 12:18:14 +0200

    Add Guild class to store guild data from dc and db

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Arr;
 
-use app\helpers\guilds;
+use app\helpers\guilds\Guild as Guild;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
