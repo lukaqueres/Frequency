e54c8b8ce46ee1637dd2606574272718382df16a
lukaqueres <lkocieln@gmail.com>
2022-05-29 15:10:00 +0200

    Debugging Guild Class

--- web/config/app.php
+++ web/config/app.php
@@ -192,6 +192,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        ‘Guild’ => App\Helpers\Guild::class,
     ])->toArray(),
 
 ];
