7ca80fdd3fcb927ec78f81d1bdd26e5100de4091
lukaqueres <lkocieln@gmail.com>
2022-06-05 13:32:23 +0200

    Style guild wiew

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -109,6 +109,15 @@
                     ?>
                     </div>
                 </div>
+                <div class="card">
+                    <p>Overwiew</p>
+                    <ul class="no-points">
+                        <li><span> Name </span> <?php echo '<span class="bg-text">' . $guild->name . '</span>' ; ?> </li>
+                        <li><span> Id </span> <?php echo '<span class="bg-text">' . $guild->id . '</span>' ; ?> </li>
+                        <li><span> Bot </span> <?php echo '<span class="bg-text">' . $s . '</span>' ; ?> </li>
+                        <li><span> Role </span> <?php echo '<span class="bg-text">' . $guild->role . '</span>' ; ?> </li>
+                    </ul>
+                </div>
             </div>
                 <h5>
                 <?php
