9a1c133770aa1ea07a1d82cd05024c76f2149167
lukaqueres <lkocieln@gmail.com>
2022-06-14 16:19:47 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -186,7 +186,7 @@
         input.name = "key-word";
 
         //button.onclick = function() { removeInput('"key-word-"'+(count + 1));};
-        button.setAttribute("onclick",'removeInput("key-word-"'+(count + 1)'")');
+        button.setAttribute("onclick",'removeInput("key-word-"'+(count + 1)+'")');
         //button.HTML = '<button class="text" onclick="key-word-' + (count+1) + '">Remove key-word</button>'
         button.innerText = "Remove key-word";
         button.className = "text";
