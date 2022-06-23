099b625dff5721979a4f625db6e987c1b659ef28
lukaqueres <lkocieln@gmail.com>
2022-06-23 20:29:01 +0200

    Update debug.blade.php

--- web/resources/views/debug.blade.php
+++ web/resources/views/debug.blade.php
@@ -10,31 +10,24 @@
 	    <meta charset="UTF-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+    <style>
+        #this-random-picker {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
     <body>
-        Main</br>
-        <a href='https://discord.com/api/oauth2/authorize?client_id=875271995644842004&state=%2Fmanage%2Fguild%2F640181649463705650&redirect_uri=https%3A%2F%2Fweb-plan-it.herokuapp.com%2Fdiscord%2Fauthorize&response_type=code&scope=identify%20email%20guilds%20guilds.members.read'> AUTHORIZE FOR GUILD</a>
-        <a href='https://discord.com/api/oauth2/authorize?client_id=875271995644842004&redirect_uri=https%3A%2F%2Fweb-plan-it.herokuapp.com%2Fdiscord%2Fauthorize&response_type=code&scope=identify%20email%20guilds%20guilds.members.read'> AUTHORIZE </a>
-        <div id="notification-container" class="flex vertical">
-            <?php
-                if (Session::exists('notification')) {
-                    $notification = Session::get('notification');
-                    $notifynode = $notification->node;
-                    echo $notifynode;
-                }
-            ?>
-            <div class="notification">
-                <p>User not authorized</p>
-                <h5>Authorize to continue</h5>
-                <button class="close" onclick="closeParent(event)">Close notification</button>
-            </div>
-            <div class="notification">
-                <h5>status</h5>
-                <button class="close" onclick="closeParent(event)">Close notification</button>
-            </div>
-            <div class="notification">
-                <h5>status</h5>
-                <button class="close" onclick="closeParent(event)">Close notification</button>
+        DEBUGGING
+        <div id="this-random-picker">
+            <div class="flex">
+                <input type="text" id="this-provide-text" placeholder="Thing to random"/>
+                <input type="range" id="this-provide-range" value='100'  min='10' max='100'/>
+                <button onclick="">Add possibility</button>
             </div>
         </div>
     </body>
+
+    <script>
+
+    </script>
 </html>
