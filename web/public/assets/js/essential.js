e373f932772c24b25cb1567d27836df2bd0bd868
lukaqueres <lkocieln@gmail.com>
2022-06-27 17:28:35 +0200

    Add finction for closing multiple DropDowns

---
+++ web/public/assets/js/essential.js
@@ -0,0 +1,22 @@
+const pass = () => {}
+
+function OpenDropdown(e) {
+    var container = e.target.parentNode;
+    var content = container.querySelector(".content");
+    content.classList.add("show");
+}
+
+function CloseDropdownOnClick(e) {
+    var target = e.target;
+    var menus = document.querySelector(".menu .content");
+    var activeMenus = menus.querySelector(".show");
+    for (const menu of activeMenus) {
+        if (menu == target || menu.contains(target)) {
+            pass();
+        } else {
+            if (menu.classList.contains('show')) {
+                content.classList.remove("show");
+            }
+        }
+    }
+}
