c2a6a6547097dbe1150b74de8c552ed811006590
lukaqueres <lkocieln@gmail.com>
2022-06-28 20:03:00 +0200

    Update essential.js

--- web/public/assets/js/essential.js
+++ web/public/assets/js/essential.js
@@ -45,7 +45,7 @@ function OpenDropdown(e) {
         container = container.parentNode;
     }
     var content = container.querySelectorAll('content');
-    console.log('Container: ' + container + ' ,  Content: ' + content + ' ,  First element: ' + content.item(0);
+    console.log('Container: ' + container + ' ,  Content: ' + content + ' ,  First element: ' + content.item(0));
     content.classList.add("show"); 
 }
 
