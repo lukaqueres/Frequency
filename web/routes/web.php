6c7e206197e605f9d0dc9a0051e5ca0bd93d721d
lukaqueres <lkocieln@gmail.com>
2022-07-20 16:40:58 +0200

    Update web.php

--- web/routes/web.php
+++ web/routes/web.php
@@ -18,7 +18,7 @@ Route::get( '/manage/', 'DiscordController@showManage' )->name('manage');
 Route::get('/', 'MainController@index');
 
 Route::get('/manage/{view?}', 'DiscordController@showManage');
-Route::get('/manage/guild/{id}/{view?}', 'DiscordController@Guildview');
+Route::get('/manage/guild/{id}/{view?}', 'DiscordController@showGuild');
 Route::get('/manage/user/{view?}', 'DiscordController@showUser');
 
 Route::match(['get', 'post'], '/discord/authorize', 'DiscordController@authorizeMe');
