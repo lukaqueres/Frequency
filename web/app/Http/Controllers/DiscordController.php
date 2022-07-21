6d52511d6b2a6299db20865a2d64ddc30f1511b0
lukaqueres <lkocieln@gmail.com>
2022-07-21 10:05:09 +0200

    Update DiscordController.php

--- web/app/Http/Controllers/DiscordController.php
+++ web/app/Http/Controllers/DiscordController.php
@@ -322,7 +322,7 @@ class DiscordController extends Controller
         $data = array();
         $data['id']=$request->id;
         $data['view']=$request->view;
-        $views = array('overview', 'settings', 'text-settings', 'debug');
+        $views = array('', 'overview', 'settings', 'text-settings', 'debug');
         $selected_view = $request->view;
         if (in_array($selected_view, $views)) {
             return view('guild', $data);
