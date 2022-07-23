a9c7c0bd3f3fa05e9acefd78eb8981b9428e8d8c
lukaqueres <lkocieln@gmail.com>
2022-07-23 19:46:06 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -210,10 +210,10 @@
                         </div>
                         <div class="card">
                             <ul class="no-points">
-                                <li><input type="checkbox" id="filter_messages" name="filter_messages"/><span>Filter messages</span> <?php echo '<span class="bg-text">' . $guild->name . '</span>' ; ?> </li>
-                                <li><input type="checkbox" id="notify_flagged" name="notify_flagged"/><span>Notify about flagged messages</span> <?php echo '<span class="bg-text">' . $guild->id . '</span>' ; ?> </li>
-                                <li><input type="checkbox" id="delete_flagged" name="delete_flagged"/><span>Delete flagged messages</span> <?php echo '<span class="bg-text">' . $s . '</span>' ; ?> </li>
-                                <li><input type="checkbox" id="delete_invites" name="delete_invites"/><span>Delete discord invites</span> <?php echo '<span class="bg-text">' . $guild->role . '</span>' ; ?> </li>
+                                <li><input type="checkbox" id="filter_messages" name="filter_messages"/><span>Filter messages</span></li>
+                                <li><input type="checkbox" id="notify_flagged" name="notify_flagged"/><span>Notify about flagged messages</span></li>
+                                <li><input type="checkbox" id="delete_flagged" name="delete_flagged"/><span>Delete flagged messages</span></li>
+                                <li><input type="checkbox" id="delete_invites" name="delete_invites"/><span>Delete discord invites</span></li>
                             </ul>
                         </div>
                     </div>
