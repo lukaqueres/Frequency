49ff8c6c1f0bdad1b3b597a0645315a4a779f3d5
lukaqueres <lkocieln@gmail.com>
2022-08-01 18:46:55 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -362,4 +362,25 @@
         }
     }
 
+
+    function txtSettingsView(object) {
+    const xhttp = new XMLHttpRequest();
+    let obj = document.getElementById(object);
+    xhttp.onload = function () {
+        obj.innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "/data/guild/<?php echo $guild->id; ?>?operation=getview&view=textSettings");
+    xhttp.send();
+    goTo("Text Settings", "Plan It | Txt settings", '/manage/guild/<?php echo $id; ?>/text-settings');
+}
+
+    function AJAXtest() { //TEST
+        const xhttp = new XMLHttpRequest();
+        let test = document.getElementById("test_xyz");
+        xhttp.onload = function () {
+            test.innerHTML = this.responseText;
+        }
+        xhttp.open("GET", "/data/guild/<?php echo $guild->id; ?>?status='test'");
+        xhttp.send();
+    }
 </script>
