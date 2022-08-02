c80fdeab3ce486733484835d1283ab1a144bb219
lukaqueres <lkocieln@gmail.com>
2022-08-02 15:10:15 +0200

    Update guild.blade.php

--- web/resources/views/guild.blade.php
+++ web/resources/views/guild.blade.php
@@ -203,32 +203,7 @@
                         </div>
                     </div>
                     <?php } else if ($view == 'text-settings') { // TEXT SETTINGS VIEW ---------------------------------------------------------- ?>
-                    <div class="card-container">
-                        <div class="card huge-title full transparent">
-                            <div class="flex">
-                                <p class="x-title">Text settings</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <p class="title">General</p>
-                            <ul class="no-points">
-                                <li><input type="checkbox" id="filter_messages" name="filter_messages"/><span>Report </span></li>
-                                <li><input type="checkbox" id="notify_flagged" name="notify_flagged"/><span>Delete suspicious links</span></li>
-                                <li><input type="checkbox" id="delete_flagged" name="delete_flagged"/><span>Delete all links</span></li>
-                                <li><input type="checkbox" id="delete_invites" name="delete_invites"/><span>Delete discord invites</span></li>
-                            </ul>
-                        </div>
-                        <div class="card full card-container">
-                            <div class="card transparent">
-                                <ul class="no-points">
-                                    <li><label class="chck-label"><input type="checkbox" id="filter_messages"/><span class="label">Delete links</span></label></li>
-                                    <li><label class="chck-label"><input type="checkbox" id="notify_flagged"/><span class="label">Enable internal links filter</span></label></li>
-                                    <li><label class="chck-label"><input type="checkbox" id="delete_flagged"/><span class="label">Delete all links</span></label></li>
-                                    <li><label class="chck-label"><input type="checkbox" id="delete_invites"/><span class="label">Delete discord invites</span></label></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                        <?php include('pages/guildView_textSettings_page.php'); ?>
                     <?php } else if ($view == 'debug') { // DEBUG VIEW -------------------------------------------------------------------------- -7760928402638897752 ?>
                     <div class="card-container">
                         <div class="card huge-title full transparent">
