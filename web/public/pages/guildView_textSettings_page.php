b2fbe011ce3688b9a799c1454ed33b9693562237
lukaqueres <lkocieln@gmail.com>
2022-08-02 17:07:08 +0200

    Update guildView_textSettings_page.php

--- web/public/pages/guildView_textSettings_page.php
+++ web/public/pages/guildView_textSettings_page.php
@@ -14,19 +14,27 @@
                         <div class="card">
                             <p class="title">General</p>
                             <ul class="no-points">
-                                <li><input type="checkbox" id="filter_messages" name="filter_messages"/><span>Report </span></li>
-                                <li><input type="checkbox" id="notify_flagged" name="notify_flagged"/><span>Delete suspicious links</span></li>
-                                <li><input type="checkbox" id="delete_flagged" name="delete_flagged"/><span>Delete all links</span></li>
-                                <li><input type="checkbox" id="delete_invites" name="delete_invites"/><span>Delete discord invites</span></li>
+                                <li><input type="checkbox" id="filter_messages"/><span>Report </span></li>
+                                <li><input type="checkbox" id="notify_flagged"/><span>Delete suspicious links</span></li>
+                                <li><input type="checkbox" id="delete_flagged"/><span>Delete all links</span></li>
+                                <li><input type="checkbox" id="delete_invites"/><span>Delete discord invites</span></li>
                             </ul>
                         </div>
-                        <div class="card full card-container">
+                        <div class="card transparent full card-container">
                             <div class="card transparent">
                                 <ul class="no-points">
                                     <li><label class="chck-label"><input type="checkbox" id="filter_messages"/><span class="label">Delete links</span></label></li>
                                     <li><label class="chck-label"><input type="checkbox" id="notify_flagged"/><span class="label">Enable internal links filter</span></label></li>
                                     <li><label class="chck-label"><input type="checkbox" id="delete_flagged"/><span class="label">Delete all links</span></label></li>
                                     <li><label class="chck-label"><input type="checkbox" id="delete_invites"/><span class="label">Delete discord invites</span></label></li>
+                                </ul>
+                            </div>
+                            <div class="card transparent">
+                                <ul class="no-points">
+                                    <li><label class="chck-label"><input type="checkbox" id="blacklist_domains"/><span class="label">Blacklist domains</span></label></li>
+                                    <li><div><input type="text" id="blacklist_domains_input" placeholder="Add blacklisted domain"/></div></li>
+                                    <li><label class="chck-label"><input type="checkbox" id="whitelist_domains"/><span class="label">Whitelist domains</span></label></li>
+                                    <li><div><input type="text" id="whitelist_domains_input" placeholder="Add whitelisted domain"/></div></li>
                                 </ul>
                             </div>
                         </div>
