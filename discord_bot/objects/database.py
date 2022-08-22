1e09c44456bc3aa8fd157bc5a40b665a6ddc1ce5
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-22 22:15:15 +0200

    Update database.py

--- discord_bot/objects/database.py
+++ discord_bot/objects/database.py
@@ -8,48 +8,48 @@ from psycopg2.extensions import AsIs
 
 class DB_conn:
 	def __init__(self, table):
-		self.table = table
-		self.cursor = self.__gen_cursor()
+		self.table = table;
+		self.cursor = self.__gen_cursor();
     	
 	def __gen_cursor(self):
-		connection = os.environ.get('DATABASE_URL')
-		con = psycopg2.connect(connection)
-		cur = con.cursor()
-		return cur
+		connection = os.environ.get('DATABASE_URL');
+		con = psycopg2.connect(connection);
+		cur = con.cursor();
+		return cur;
 	
 	def __revoke_cursor(self):
 		# Check somewhere how it was handled by now and GET IT in HERE!
-		return False
+		return False;
 	
 	def add(self, payload):
-		cur = self.cursor
-		table = self.table
-		columns = list(payload.keys())
-		values = list(payload.values())
+		cur = self.cursor;
+		table = self.table;
+		columns = list(payload.keys());
+		values = list(payload.values());
 		cur.execute(
 			"""
 			INSERT INTO %s (%s)
 			VALUES (%s);
 			""", (table, AsIs(','.join(columns)), tuple(values))
-		)
+		);
 		
 	def update(self, condition, payload): 
-		cur = self.cursor
-		table = self.table
-		columns = list(payload.keys())
-		values = [payload[column] for column in columns]
+		cur = self.cursor;
+		table = self.table;
+		columns = list(payload.keys());
+		values = [payload[column] for column in columns];
 		for value in values:
 			if isinstance(value, dict):
-				values[values.index(value)] = json.dumps(value, indent = 4)  
-		cond_key = list(condition.keys())[0]
-		condition = list(condition.values())[0]
+				values[values.index(value)] = json.dumps(value, indent = 4)  ;
+		cond_key = list(condition.keys())[0];
+		condition = list(condition.values())[0];
 		cur.execute(
 			"""
 			UPDATE %s
 			SET %s = %s
 			WHERE %s = %s
-			""", (AsIs(table), AsIs(columns[0] if sizeof(columns) == 1 else tuple(columns)), values, cond_key, condition) # AsIs(','.join(columns))
-		)
+			""", (AsIs(table), AsIs(columns[0] if (sizeof(columns) / sizeof(int)) == 1 else tuple(columns)), values, cond_key, condition) # AsIs(','.join(columns))
+		);
 		
 	def read(self, condition):
-		print("table: " + self.table)
+		print("table: " + self.table);
