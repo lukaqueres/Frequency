99e2d131a67b81a85c064814a35fb1230bc16db9
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 17:00:37 +0200

    Update events.py

--- bot/cogs/events.py
+++ bot/cogs/events.py
@@ -1,1 +1,11 @@
+import discord, json, os,
 
+class Events(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+
+    
+    
+    
+def setup(client):
+	client.add_cog(Events(client))
