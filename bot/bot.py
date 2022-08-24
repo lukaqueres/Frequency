342193a4b1ff738271ce0c5d3abd527346455f81
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 20:33:45 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -26,7 +26,7 @@ async def on_ready():
 		developer = configuration['developer'];
 		log = developer['log'];
 	statusPool = { "online": discord.Status.online, "offline": discord.Status.offline, "idle": discord.Status.idle, "dnd": discord.Status.dnd } # - statuses available to be set as bot's status in discord - 
-	if developer['active'] and developer["active-status"] in list(statusPool.keys()):
+	if developer['active'] and developer["active-status"] in statusPool:
 		statusPool[developer["active-status"]];
 	elif developer['active'] and developer["active-status"] not in list(statusPool.keys()):
 		if log['exceptions']:
