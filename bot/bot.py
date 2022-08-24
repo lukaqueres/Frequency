55fdf2660abf811964c719c04e3280a5abf311e5
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 19:28:01 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -94,8 +94,8 @@ async def cycleStatus(alist, interval, status):
 # >---------------------------------------< COGS / EXTENSIONS LOAD >---------------------------------------< # 
 with open('configuration.json', 'r') as c: # - Open 'configuration.json' file containing work data. Fetch extensions load & log details. -
 	extensions = json.load(c)['extensions']; 
-	log = json.load(c)['developer'];
-	log = log['log'];
+	log = json.load(c);
+	log = log['developer']['log'];
 if extensions['load']:
 	loaded = []; # - Extensions loaded succesfully -
 	failed = []; # - Extensions failed to load -
