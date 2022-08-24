2a7d488e3bc8f34b7104d16e63d6f78029d80b1d
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 20:07:59 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -63,7 +63,7 @@ async def on_ready():
 		listOfGuilds = [];
 		for guild in client.guilds:
 			listOfGuilds.append(guild.id);
-		print(f'----------- APPLICATION ONLINE ----------- \n{len(listofids)} guilds')
+		print(f'----------- APPLICATION ONLINE ----------- \n{len(listofGuilds)} guilds')
 	else:
 		print('----------- APPLICATION ONLINE -----------')
 		
