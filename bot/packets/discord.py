224bfbc88fd946482ada844fa8849dfeeef9273c
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 18:11:13 +0200

    Update discord.py

--- bot/packets/discord.py
+++ bot/packets/discord.py
@@ -1,1 +1,12 @@
-
+def prefix(client, message):
+	con = get_connection()
+	cur = con.cursor()
+	cur.execute(
+		"SELECT cmdPrefix FROM SERVERS_PROPERTIES WHERE guild_id=%s",
+		(message.guild.id, )
+	)
+	row = cur.fetchone()
+	prefix = row[0]
+	con.commit()
+	#print("Prefix downloaded succesfully as '{}' on '{}' guild.".format(prefix, message.guild))
+	return prefix
