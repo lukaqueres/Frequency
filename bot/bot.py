52bdb2cc0964464ba95d6642ed9240c256a52849
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-24 21:32:38 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -43,10 +43,10 @@ async def on_ready():
 	if activities['set']:
 		if activities['list'] == 'random': # - Random list means random choice of available lists. -
 			alist = activitiesLists[random.choice(activitiesLists.keys())];
-		elif activites['list'] in activitiesLists:
+		elif activities['list'] in activitiesLists:
 			alist = activitiesLists[activites['list']];
 		elif log['exceptions']:
-			print(f"List of activities not found. Try something other than {activites['list']}");
+			print(f"List of activities not found. Try something other than {activities['list']}");
 			alist = False;
 		else:
 			alist = False;
