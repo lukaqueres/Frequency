8484b1d2bfb38e49696039b9220e9a2fd70d4931
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-25 12:29:48 +0200

    Update time.py

--- bot/packets/time.py
+++ bot/packets/time.py
@@ -1,1 +1,18 @@
+import datetime, os, json
+from datetime import datetime, date, timedelta
 
+class DateTime:
+	def __init__(self):
+		self.currentUTC = datetime.now(timezone.utc);
+		self.currentUTCTimeStamp = datetime.now(timezone.utc).timestamp() * 1000;
+
+	def __del__(self):
+		print(self.currentUTC)
+		
+	def time(self):
+		pass;
+
+	def date(self):
+		pass;
+	
+	
