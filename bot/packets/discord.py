cb42d980957d1c77023c3e2aec952aeceb5c7012
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-25 12:12:48 +0200

    Update discord.py

--- bot/packets/discord.py
+++ bot/packets/discord.py
@@ -3,7 +3,8 @@ import json, os
 # - Import database in case of error -
 from packets import database
 
-def prefix(client, message, database):
+def prefix(client, message):
+	global database;
 	properties = database.select(table = 'guilds', 
 			columns = ['properties'],
 			condition = message.guild.id 
