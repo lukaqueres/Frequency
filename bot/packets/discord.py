d1f52a3c1a04c342f80769f0629a0b9a97226880
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-26 13:08:46 +0200

    Update discord.py

--- bot/packets/discord.py
+++ bot/packets/discord.py
@@ -4,10 +4,19 @@ import json, os
 from packets.database import Database
 
 def prefix(client, message):
-	database = Database(); # - TODO: Check hov to get database object from bot.py main file, for now this will do -
-	properties = database.select(table = 'guilds', 
+	with open('./configuration.json', 'r') as c: # - Open 'configuration.json' json file. Getting logging details. -
+		configuration = json.load(c); 
+		log = configuration['developer']['log'];
+		defaults = configuration['values']['defaults'];
+	try:
+		database = Database(); # - TODO: Check how to get database object from bot.py main file, for now this will do -
+		properties = database.select(table = 'guilds', 
 			columns = ['properties'],
 			condition = {"id": message.guild.id}
 			);
-	prefix = properties['prefix'];
+		prefix = properties['prefix'];
+	except Exception as e:
+		if log['exceptions']:
+			prefix = defaults['prefix'];
+			print(f'Error while getting prefix: {getattr(e, 'message', repr(e))}');
 	return prefix;
