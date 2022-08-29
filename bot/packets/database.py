46947012dddb7c1aad662e1c90cffd712f22daf4
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-08-29 18:53:04 +0200

    Update database.py

--- bot/packets/database.py
+++ bot/packets/database.py
@@ -235,6 +235,7 @@ class Escape():
 	def __indexes(self, string, lookingFor, start = 0):
 		string = string[start:];
 		indexes = [str(i) for i, x in enumerate(string, start) if x == lookingFor];
+		print(list(enumerate(string, start)));
 		return indexes;
 		
 	def all(self, values):
