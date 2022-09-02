2db450916171f403ceccdc07e1bd22547d004d94
Patryk Kocielnik <patryk@kocielnik.pl>
2022-09-02 09:19:33 +0200

    Adapt insert() for pushing multiple columns.

--- test/test_bot.py
+++ test/test_bot.py
@@ -4,24 +4,34 @@ from psycopg2.extensions import AsIs
 from discord_bot.functions import get_time
 
 
-def insert(table, payload):
+def get_insert_query(table, payload):
     url = os.environ.get('DATABASE_URL')
     con = psycopg2.connect(url, sslmode='require')
     cur = con.cursor()
     columns = list(payload.keys())
     values = [payload[column] for column in columns]
-    print(AsIs(table))
-    print(columns[0])
-    print(values[0])
     return cur.mogrify(
-        "INSERT INTO %s (%s) VALUES (%s);",
-        (AsIs(table), columns[0], values[0])
+        "INSERT INTO %s %s VALUES %s;",
+        (AsIs(table), tuple(columns), tuple(values))
     )
 
 def test_bot():
     assert str == type(get_time(specify="DT", return_type="str"))
 
-def test_insert():
+def test_insert_one_value():
     payload = {"mycolumn": ["myvalue"]}
-    query = insert("users", payload)
-    assert query == b"INSERT INTO users ('mycolumn') VALUES (ARRAY['myvalue']);"
+    query = get_insert_query("users", payload)
+    assert query == \
+        b"INSERT INTO users ('mycolumn') " \
+        b"VALUES (ARRAY['myvalue']);"
+
+def test_insert_two_columns():
+    payload = {
+        "mycolumn": ["myvalue"],
+        "mycolumntwo": ["myvaluetwo"]
+    }
+    query = get_insert_query("users", payload)
+    expected_query = \
+        b"INSERT INTO users ('mycolumn', 'mycolumntwo') " \
+        b"VALUES (ARRAY['myvalue'], ARRAY['myvaluetwo']);"
+    assert expected_query == query
