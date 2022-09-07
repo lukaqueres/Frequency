92ea6f6747bb03a43450f10607e0073349155c38
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-07 18:39:30 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -80,7 +80,7 @@ async def on_ready():
 		client.loop.create_task(cycleStatus(alist = alist, interval = activities['cycle-interval'], status = status))
 	
 	# - Sync slash commands tree to global -
-	await tree.sync()
+	await client.tree.sync()
 
 # - Function to change activity in random time interals -
 async def cycleStatus(alist, interval, status):
