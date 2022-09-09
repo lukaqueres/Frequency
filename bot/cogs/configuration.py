12985499f9c94b2cb573168d0415d6a072d2920d
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-09 17:35:03 +0200

    Update configuration.py

--- bot/cogs/configuration.py
+++ bot/cogs/configuration.py
@@ -18,7 +18,7 @@ class Configuration(app_commands.Group, name="configuration", description="Bots 
 		embed.set_thumbnail(url=self.client.user.avatar)
 		embed.add_field( name=chr(173), value="Provided by [lukaqueres](https://github.com/lukaqueres)", inline=True),
 		embed.set_footer(text="Provided by [lukaqueres](https://github.com/lukaqueres)")
-		await interaction.response.send(embed=embed, ephemeral=True)
+		await interaction.response.send_message(embed=embed, ephemeral=True)
 		
 	@app_commands.command(name="show")
 	@commands.has_permissions(administrator = True)
