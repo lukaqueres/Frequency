f43a78a6e62f58b23c148d69e4a29f834afe1570
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-09 18:04:49 +0200

    Update configuration.py

--- bot/cogs/configuration.py
+++ bot/cogs/configuration.py
@@ -2,6 +2,8 @@ import discord
 from discord import app_commands, Embed
 from discord.ext import commands
 
+from packets.time import Time
+
 class Configuration(app_commands.Group, name="configuration", description="Bots basic configuration commands."): # commands.GroupCog
 	def __init__(self, client: commands.Bot) -> None:
 		self.client = client
@@ -11,13 +13,16 @@ class Configuration(app_commands.Group, name="configuration", description="Bots 
 	@commands.has_permissions(administrator = True)
 	async def conf_sub_refresh(self, interaction: discord.Interaction) -> None:
 		""" Refresh guild data for service configuration """
+		with open('configuration.json', 'r') as c: # - Open 'configuration.json' file containing work data. Fetch extensions load & log details. -
+			configuration = json.load(c); 
+			appName = configuration['name'];
 		embed = Embed(title="User information",
 				colour = interaction.user.colour,
-				#timestamp=get_time()
+				timestamp=Time.UTCNow();
 				)
 		embed.set_thumbnail(url=self.client.user.avatar)
-		embed.add_field( name=chr(173), value="Provided by [lukaqueres](https://github.com/lukaqueres)", inline=True),
-		embed.set_footer(text="Provided by [lukaqueres](https://github.com/lukaqueres)")
+		embed.add_field( name=chr(173), value="Made by [lukaqueres](https://github.com/lukaqueres)", inline=True),
+		embed.set_footer(text=f"Provided by {appName}")
 		await interaction.response.send_message(embed=embed, ephemeral=True)
 		
 	@app_commands.command(name="show")
