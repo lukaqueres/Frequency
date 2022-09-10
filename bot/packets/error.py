8a687a769c6c1fe0b845163bd32d018b1ee86053
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-10 21:54:57 +0200

    Update error.py

--- bot/packets/error.py
+++ bot/packets/error.py
@@ -7,8 +7,8 @@ from packets.discord import PIEmbed
 
 class CommandOnCooldown(commands.CommandError):
     def __init__(self, command, cooldown, Optional(interaction), Optional(ctx), *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.command = command
         self.cooldown = cooldown
         self.interaction = interaction or None
         self.ctx = ctx or None
-        super().__init__(*args, **kwargs)
