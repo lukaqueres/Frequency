ab6ff08ce46bef33be60d7e61cde885db9c22ab5
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-10 22:09:25 +0200

    Update error.py

--- bot/packets/error.py
+++ bot/packets/error.py
@@ -6,9 +6,24 @@ from typing import Optional
 from packets.discord import PIEmbed
 
 class CommandOnCooldown(commands.CommandError):
-    def __init__(self, command, cooldown, Optional(interaction), Optional(ctx), *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.command = command
-        self.cooldown = cooldown
-        self.interaction = interaction or None
-        self.ctx = ctx or None
+	def __init__(self, command, cooldown, Optional(interaction), Optional(ctx), *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.command = command
+		self.cooldown = cooldown
+		self.interaction = interaction or None
+		self.ctx = ctx or None
+
+class NotEnoughBotPermissions(commands.CommandError):
+	def __init__(self, permission, Optional(interaction), Optional(ctx), *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.permission = permission
+		self.interaction = interaction or None
+		self.ctx = ctx or None
+
+class NotEnoughUserPermissions(commands.CommandError):
+	def __init__(self, user, permission, Optional(interaction), Optional(ctx), *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.user = user
+		self.permission = permission
+		self.interaction = interaction or None
+		self.ctx = ctx or None
