65233a61a69edd34257ee95436fa68945698caf6
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-12 22:08:09 +0200

    Update configuration.py

--- bot/cogs/configuration.py
+++ bot/cogs/configuration.py
@@ -46,7 +46,6 @@ class ConfigurationGroup(app_commands.Group, name="configuration", description="
 	@app_commands.command(name="refresh", description="Check for accurate & refresh guild data for service configuration")
 	@commands.has_permissions(administrator = True)
 	async def conf_sub_refresh(self, interaction: discord.Interaction) -> None:
-		dfhhdfff[xd][sddadsd90sdad];
 		if not self.__commands_check(interaction):
 			return print('stopping')
 		""" Check for accurate & refresh guild data for service configuration """
@@ -60,7 +59,6 @@ class ConfigurationGroup(app_commands.Group, name="configuration", description="
 			description="Guild data synchronize check will be performed before refreshing records."
 		);
 		embed.set_thumbnail(url=self.client.user.avatar)
-		dfhhdfff[xd][sddadsd90sdad];
 		DBRoles = self.client.database.select(
 			table = 'guilds.properties', 
 			condition = {"id": interaction.guild_id}, 
