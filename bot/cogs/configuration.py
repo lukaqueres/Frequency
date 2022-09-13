89ffd450b14efcf06362f7ee558b94401fa5a809
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-13 20:45:48 +0200

    Update configuration.py

--- bot/cogs/configuration.py
+++ bot/cogs/configuration.py
@@ -109,11 +109,18 @@ class ConfigurationGroup(app_commands.Group, name="configuration", description="
 					    payload = payload);
 			
 		title = ''
-		content = ''
-		for i in range(200):
-			content += 'l'
-		for i in range(2000):
-			content += 'x'
+		content = """
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+		Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+		scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+		remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+		and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+		The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+		content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as 
+		their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have 
+		evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+		"""
 		len(title)
 		len(content)
 		embed.add.field(title="xxxxxxxxxxxfvjufbjnufbsijobunbfjnbfjnbfjnfb", content=content);
