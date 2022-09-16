5ca8c5d9cacd5394072ae54d0e144e69aa50524e
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-16 17:15:07 +0200

    Update utilities.py

--- bot/packets/utilities.py
+++ bot/packets/utilities.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from packages.discord import PIBot 
+from packets.discord import PIBot 
 
 class Checks:
 	def __init__(self, client: PIBot) -> None:
