ab182aea99791cb277044734c3035adeee624d83
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-17 22:38:11 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -56,86 +56,89 @@ asyncio.run(startup());
 # >---------------------------------------< ON application ACTIVE >---------------------------------------< # 
 @client.event
 async def on_ready():
-	with open('configuration.json', 'r') as c: # - Open 'configuration.json' json file. Getting status, logging and activities. -
-		configuration = json.load(c); 
-		status = configuration['discord']['status']; 
-	statuses = { "online": discord.Status.online, "offline": discord.Status.offline, "idle": discord.Status.idle, "dnd": discord.Status.dnd } # - statuses available to be set as bot's status in discord - 
-	activitiesList = { "watching": discord.ActivityType.watching, "listening": discord.ActivityType.listening} # - Available activities types, `playing` not included due to diffrent setup procedure -
-	if self.configuration.read(category="overview", key="developer.active"):
-		status = self.configuration.read(category="overview", key="developer.discord-status")
-		if status not in list(statuses.keys()):
-			raise ValueError("`{}` status is not supported, try instead: {}".format(status, ', '.join(list(statusPool.keys()))));	
-		status = statuses[status]
-	elif self.configuration.read(category="overview", key="discord.status.set") not in list(statuses.keys()): # - Checking if status given in json file is correct for use, assigning python code if apply, set to online if not. -
-		raise ValueError("`{}` status is not supported, try instead: {}".format(status, ', '.join(list(statusPool.keys()))));
-	else:	 
-		status = statuses[self.configuration.read(category="overview", key="discord.status.set")];
-		# - End of custom status assign. -
-		
-	# - Assingning custom activity status to bot -
-	if self.configuration.read(category="overview", key="discord.activity.set"):
-		activities = self.configuration.read(category="overview", key="discord.activity.list");
-		pool = self.configuration.read(category="overview", key="discord.activity.pool");
+	try:
+		with open('configuration.json', 'r') as c: # - Open 'configuration.json' json file. Getting status, logging and activities. -
+			configuration = json.load(c); 
+			status = configuration['discord']['status']; 
+		statuses = { "online": discord.Status.online, "offline": discord.Status.offline, "idle": discord.Status.idle, "dnd": discord.Status.dnd } # - statuses available to be set as bot's status in discord - 
+		activitiesList = { "watching": discord.ActivityType.watching, "listening": discord.ActivityType.listening} # - Available activities types, `playing` not included due to diffrent setup procedure -
 		if self.configuration.read(category="overview", key="developer.active"):
-			activities = self.configuration.read(category="overview", key="developer.discord-custom-sctivity");
-		elif activities == 'random': # - Random list means random choice of available lists. -
-			activities = pool[random.choice(list(pool.keys()))];
-		elif activities in list(pool.keys()):
-			activities = pool[activities];
-		else:
-			raise ValueError("`{}` activities list not found".format(activities));
-		if len(activities['list']) == 0:
-			raise ValueError("List of custom statuses can not be empty, set activity to false in such case");	 
-		activity = random.choice(activities['list'])
-		if activities['type'] == 'playing': # - Set special statuses: 'Playing something' or 'Watching something' etc. -
-			await client.change_presence(status=status, activity=discord.Game(activity));
-		elif activities['type'] in list(activitiesList.keys()):
-			await bot.change_presence(activity=discord.Activity(type=activitiesList[activities['type']], name=activity));
-		else:
-			raise ValueError("{} is not a valid activity type".format(activities['type']));
-	else:
-		await client.change_presence(status=status); # - Change only status if activities are not meant to be set -
-	
-	clientGuildsIds = [];
-	for guild in client.guilds:
-		clientGuildsIds.append(guild.id);
-	client.log.hard('- - - - - - - - - - - APPLICATION ONLINE - - - - - - - - - - -')
-	client.log.notify('{} guilds; status: {}; activity: {}'.format(len(clientGuildsIds), status, activity or 'None'))
-	if self.configuration.read(category="overview", key="discord.activity.set") and self.configuration.read(category="overview", key="discord.activity.cycle"):
-		client.log.notify("Activity changing from pool: {} in interval: {}".format(', '.join(activities['list']), self.configuration.read(category="overview", key="discord.activity.cycle-interval")));
-		client.loop.create_task(cycleStatus(activities = activities, interval = self.configuration.read(category="overview", key="discord.activity.cycle-interval"), status = status))
-	
-	# - Sync slash commands tree to global -
-	await client.tree.sync()
+			status = self.configuration.read(category="overview", key="developer.discord-status")
+			if status not in list(statuses.keys()):
+				raise ValueError("`{}` status is not supported, try instead: {}".format(status, ', '.join(list(statusPool.keys()))));	
+			status = statuses[status]
+		elif self.configuration.read(category="overview", key="discord.status.set") not in list(statuses.keys()): # - Checking if status given in json file is correct for use, assigning python code if apply, set to online if not. -
+			raise ValueError("`{}` status is not supported, try instead: {}".format(status, ', '.join(list(statusPool.keys()))));
+		else:	 
+			status = statuses[self.configuration.read(category="overview", key="discord.status.set")];
+			# - End of custom status assign. -
 
-#@on_ready.error
-#async def on_ready_error(error):
-#	print('Error ' + str(type(error)) + ', '.join(list(inst.args)))
-#	traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+		# - Assingning custom activity status to bot -
+		if self.configuration.read(category="overview", key="discord.activity.set"):
+			activities = self.configuration.read(category="overview", key="discord.activity.list");
+			pool = self.configuration.read(category="overview", key="discord.activity.pool");
+			if self.configuration.read(category="overview", key="developer.active"):
+				activities = self.configuration.read(category="overview", key="developer.discord-custom-sctivity");
+			elif activities == 'random': # - Random list means random choice of available lists. -
+				activities = pool[random.choice(list(pool.keys()))];
+			elif activities in list(pool.keys()):
+				activities = pool[activities];
+			else:
+				raise ValueError("`{}` activities list not found".format(activities));
+			if len(activities['list']) == 0:
+				raise ValueError("List of custom statuses can not be empty, set activity to false in such case");	 
+			activity = random.choice(activities['list'])
+			if activities['type'] == 'playing': # - Set special statuses: 'Playing something' or 'Watching something' etc. -
+				await client.change_presence(status=status, activity=discord.Game(activity));
+			elif activities['type'] in list(activitiesList.keys()):
+				await bot.change_presence(activity=discord.Activity(type=activitiesList[activities['type']], name=activity));
+			else:
+				raise ValueError("{} is not a valid activity type".format(activities['type']));
+		else:
+			await client.change_presence(status=status); # - Change only status if activities are not meant to be set -
+
+		clientGuildsIds = [];
+		for guild in client.guilds:
+			clientGuildsIds.append(guild.id);
+		client.log.hard('- - - - - - - - - - - APPLICATION ONLINE - - - - - - - - - - -')
+		client.log.notify('{} guilds; status: {}; activity: {}'.format(len(clientGuildsIds), status, activity or 'None'))
+		if self.configuration.read(category="overview", key="discord.activity.set") and self.configuration.read(category="overview", key="discord.activity.cycle"):
+			client.log.notify("Activity changing from pool: {} in interval: {}".format(', '.join(activities['list']), self.configuration.read(category="overview", key="discord.activity.cycle-interval")));
+			client.loop.create_task(cycleStatus(activities = activities, interval = self.configuration.read(category="overview", key="discord.activity.cycle-interval"), status = status))
+
+		# - Sync slash commands tree to global -
+		await client.tree.sync()
+	except Exception as error:
+		print('Error ' + str(type(error)) + ', '.join(list(inst.args)))
+		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 	
 # - Function to change activity in random time interals -
 async def cycleStatus(activities, interval, status):
-	activitiesList = { "watching": discord.ActivityType.watching, "listening": discord.ActivityType.listening} # - Available activities types, `playing` not included due to diffrent setup procedure -
-	await client.wait_until_ready()
-	while not client.is_closed():
-		if isinstance(interval, int):
-			time = interval;
-		elif interval == 'random': # - Interval draw -
-			time = random.randint(900,7200);
-		elif interval == 'short':
-			time = random.randint(900,2700);
-		elif interval == 'long':
-			time = random.randint(2700,7200);
-		else: # - In case of not appropriate interval given ( not: short, long, random ). -
-			raise ValueError("{} is not a valid interval".format(interval));
-		activity = random.choice(activities[list]);
-		if activities['type'] == 'playing': # - Set special statuses: 'Playing something' or 'Watching something' etc. -
-			await client.change_presence(status=status, activity=discord.Game(activity));
-		elif activities['type'] in list(activitiesList.keys()):
-			await bot.change_presence(activity=discord.Activity(type=activitiesList[activities['type']], name=activity));
-		if log['notices']:
-			client.log.notify("Changed activity: {}; Waiting {} seconds.".format(activity, time))
-		await asyncio.sleep(time) # - Wait interval. -
+	try:
+		activitiesList = { "watching": discord.ActivityType.watching, "listening": discord.ActivityType.listening} # - Available activities types, `playing` not included due to diffrent setup procedure -
+		await client.wait_until_ready()
+		while not client.is_closed():
+			if isinstance(interval, int):
+				time = interval;
+			elif interval == 'random': # - Interval draw -
+				time = random.randint(900,7200);
+			elif interval == 'short':
+				time = random.randint(900,2700);
+			elif interval == 'long':
+				time = random.randint(2700,7200);
+			else: # - In case of not appropriate interval given ( not: short, long, random ). -
+				raise ValueError("{} is not a valid interval".format(interval));
+			activity = random.choice(activities[list]);
+			if activities['type'] == 'playing': # - Set special statuses: 'Playing something' or 'Watching something' etc. -
+				await client.change_presence(status=status, activity=discord.Game(activity));
+			elif activities['type'] in list(activitiesList.keys()):
+				await bot.change_presence(activity=discord.Activity(type=activitiesList[activities['type']], name=activity));
+			if log['notices']:
+				client.log.notify("Changed activity: {}; Waiting {} seconds.".format(activity, time))
+			await asyncio.sleep(time) # - Wait interval. -
+	except Exception as error:
+		except Exception as error:
+		print('Error ' + str(type(error)) + ', '.join(list(inst.args)))
 
 # >---------------------------------------< COMMANDS >---------------------------------------< # 
 		
