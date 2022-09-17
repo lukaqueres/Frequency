b13aa6e5c7e139a62d36ec3cdc61d2ee9fcc4713
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-17 15:47:20 +0200

    Update utilities.py

--- bot/packets/utilities.py
+++ bot/packets/utilities.py
@@ -1,48 +1,84 @@
-import discord
+import discord, traceback, sys, os
 from typing import Optional
 
 from discord import app_commands
 from discord.ext import commands
 
 from packets.discord import PIBot 
-
-class Checks:
-	def __init__(self, client: PIBot) -> None:
-		self.client = client
-
-	def __has_permission(self, permission):
-		
-	def has_privileges(self, **perms) -> commands.check: 
-		if ctx:
-			async def predicate(ctx):
-				# Add the perms to a variable, you can format this any way you'd want
-				ctx.command.required_perms = [p.replace('_', ' ').title() for p in perms.keys()]
-				# Just use the normal check
-				return await commands.has_permissions(**perms).predicate(ctx)
-		else:
-			async def predicate(ctx):
-				# Add the perms to a variable, you can format this any way you'd want
-				interaction.command.required_perms = [p.replace('_', ' ').title() for p in perms.keys()]
-				# Just use the normal check
-				return await commands.has_permissions(**perms).predicate(ctx)
-			
-		return commands.check(predicate)
-			   
-	def is_admin(self):
-		def predicate(interaction: discord.Interaction = Null, ctx: commands.Context = Null):
-			if ctx:
-				return ctx.message.author.guild_permissions.administrator
-			else:
-				return interaction.user.guild_permissions.administrator
-		return commands.check(predicate)
 	
-class CheckCooldown(Checks):
-	def __init__(self, client: PIBot, check) -> None:
-		super().__init__(client)
-		self.cooldown = self.cooldown = commands.CooldownMapping.from_cooldown(1, 600, commands.BucketType.guild)
+class File:
+	def __init__(self):
+		pass;
+		#self.file = 'configuration/{}.json'
 		
-	def update(self):
-		retry = self.cooldown.get_bucket(interaction).update_rate_limit();
-		if retry:
-			return False;
-		return True
+	def write(file, text:str, mode:str = 'a'):
+		with open(file, mode) as w; 
+				w.seek(0, os.SEEK_END); 
+				w.write(text);
+	
+class Configuration:
+	def __init__(self):
+		self.file = 'configuration/{}.json'
+		
+	def read(self, category:str, key):
+		file = self.file.format(category)
+		try:
+			with open(file, 'r') as c: # - Open 'configuration.json' file containing work data. Fetch extensions load & log details. -
+				configuration = json.load(c); 
+		except error as error:
+			traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+			
+		request = configuration[key]
+		return request;
+	
+class Logger:
+	def __init__(self, client, file:str = None):
+		self.configuration = client.configuration
+		self.time = client.time
+		self.logFile = file
+		self.file = File();
+		self.print = self.configuration.read(category="utilities", key="log.print") 
+		self.save = self.configuration.read(category="utilities", key="log.save_to_file")
+		self.addTimestamp = self.configuration.read(category="utilities", key="log.add_time_stamp")
+		
+	def exception(self, text) -> None:
+		log = self.configuration.read(category="utilities", key="log.exceptions")
+		text = self.time.UTCNow() if self.addTimestamp else '' + ' >>> EXCEPTION: ' + text
+		if not log:
+			return;
+		if log and self.print:
+			print(text);
+		if log and self.save: 
+			self.file.write(file = self.logFile, text = text)
+		
+	def notify(self, text) -> None:
+		log = self.configuration.read(category="utilities", key="log.notices")
+		text = self.time.UTCNow() if self.addTimestamp else '' + ' >>> NOTICE: ' + text
+		if not log:
+			return;
+		if log and self.print:
+			print(text);
+		if log and self.save: 
+			self.file.write(file = self.logFile, text = text)
+				
+	def error(self, text) -> None:
+		log = self.configuration.read(category="utilities", key="log.errors")
+		text = self.time.UTCNow() if self.addTimestamp else '' + ' >>> ERROR: ' + text
+		if not log:
+			return;
+		if log and self.print:
+			print(text);
+		if log and self.save: 
+			self.file.write(file = self.logFile, text = text)
+				
+	def debug(self, text) -> None:
+		log = self.configuration.read(category="utilities", key="log.debug")
+		text = self.time.UTCNow() if self.addTimestamp else '' + ' >>> DEBUG: ' + text
+		if not log:
+			return;
+		if log and self.print:
+			print(text);
+		if log and self.save: 
+			self.file.write(file = self.logFile, text = text)
+				
+		
