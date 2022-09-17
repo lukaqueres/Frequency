099f02912a6a0029a51cea5a0a19a9aeaf13f37e
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-17 11:21:52 +0200

    Update and rename commandErrorHandler.py to errors.py

---
+++ bot/cogs/errors.py
@@ -0,0 +1,174 @@
+import discord, traceback, sys
+
+from discord.ext import commands
+from discord import app_commands
+
+from packets.discord import PIBot
+from packets.error import *
+
+class Errors(commands.Cog, name="errors"):
+	"""Errors handler."""
+	def __init__(self, bot: PIBot) -> None:
+		self.bot = bot
+		bot.tree.error(coro = self.__dispatch_to_app_command_handler)
+
+		self.default_error_message = ">>> There was an error while executing command."
+
+	"""def help_custom(self):
+		emoji = "<a:crossmark:842800737221607474>"
+		label = "Error"
+		description = "A custom errors handler. Nothing to see here."
+		return emoji, label, description"""
+
+	def trace_error(self, level: str, error: Exception):
+		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+		#self.bot.logger.name = f"discord.{level}"
+		#self.bot.logger.error(msg=type(error).__name__, exc_info=error)
+		
+		raise error
+
+	async def __dispatch_to_app_command_handler(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+		self.bot.dispatch("app_command_error", interaction, error)
+
+	async def __respond_to_interaction(self, interaction: discord.Interaction) -> bool:
+		try:
+			await interaction.response.send_message(content=self.default_error_message, ephemeral=True)
+			return True
+		except discord.errors.InteractionResponded:
+			return False
+
+	@commands.Cog.listener("on_error")
+	async def get_error(self, event, *args, **kwargs):
+		"""Error handler"""
+		print(f"! Unexpected Internal Error: (event) {event}, (args) {args}, (kwargs) {kwargs}.")
+
+	@commands.Cog.listener("on_command_error")
+	async def get_command_error(self, ctx: commands.Context, error: commands.CommandError):
+		"""Command Error handler
+		doc: https://discordpy.readthedocs.io/en/master/ext/commands/api.html#exception-hierarchy
+		"""
+		try:
+			if ctx.interaction: # HybridCommand Support
+				await self.__respond_to_interaction(ctx.interaction)
+				edit = ctx.interaction.edit_original_message
+				if isinstance(error, commands.HybridCommandError):
+					error = error.original # Access to the original error
+			else:
+				discord_message = await ctx.send(self.default_error_message)
+				edit = discord_message.edit
+
+			raise error
+
+		# ConversionError
+		except commands.ConversionError as d_error:
+			await edit(content=f"{d_error}")
+		# UserInputError
+		except commands.MissingRequiredArgument as d_error:
+			await edit(content=f"Something is missing. `{ctx.clean_prefix}{ctx.command.name} <{'> <'.join(ctx.command.clean_params)}>`")
+		# UserInputError -> BadArgument
+		except commands.MemberNotFound or commands.UserNotFound as d_error:
+			await edit(content=f"Member `{str(d_error).split(' ')[1]}` not found ! Don't hesitate to ping the requested member.")
+		# UserInputError -> BadUnionArgument | BadLiteralArgument | ArgumentParsingError
+		except commands.BadArgument or commands.BadUnionArgument or commands.BadLiteralArgument or commands.ArgumentParsingError as d_error:
+			await edit(content=f"{d_error}")
+		# CommandNotFound
+		except commands.CommandNotFound as d_error:
+			await edit(content=f"Command `{str(d_error).split(' ')[1]}` not found !")
+		# CheckFailure
+		except commands.PrivateMessageOnly:
+			await edit(content="This command canno't be used in a guild, try in direct message.")
+		except commands.NoPrivateMessage:
+			await edit(content="This is not working as excpected.")
+		except commands.NotOwner:
+			await edit(content="You must own this bot to run this command.")
+		except commands.MissingPermissions as d_error:
+			await edit(content=f"Your account require the following permissions: `{'` `'.join(d_error.missing_permissions)}`.")
+		except commands.BotMissingPermissions as d_error:
+			if not "send_messages" in d_error.missing_permissions:
+				await edit(content=f"The bot require the following permissions: `{'` `'.join(d_error.missing_permissions)}`.")
+		except commands.CheckAnyFailure or commands.MissingRole or commands.BotMissingRole or commands.MissingAnyRole or commands.BotMissingAnyRole as d_error:
+			await edit(content=f"{d_error}")
+		except commands.NSFWChannelRequired:
+			await edit(content="This command require an NSFW channel.")
+		# DisabledCommand
+		except commands.DisabledCommand:
+			await edit(content="Sorry this command is disabled.")
+		# CommandInvokeError
+		except commands.CommandInvokeError as d_error:
+			await edit(content=f"{d_error.original}")
+		# CommandOnCooldown
+		except commands.CommandOnCooldown as d_error:
+			await edit(content=f"Command is on cooldown, wait `{str(d_error).split(' ')[7]}` !")
+		# MaxConcurrencyReached
+		except commands.MaxConcurrencyReached as d_error:
+			await edit(content=f"Max concurrency reached. Maximum number of concurrent invokers allowed: `{d_error.number}`, per `{d_error.per}`.")
+		# HybridCommandError
+		except commands.HybridCommandError as d_error:
+			await self.get_app_command_error(ctx.interaction, error)
+		# - CUSTOM ERRORS -
+		#except CommandOnCooldown as d_error:
+		#	return await d_error.interaction.response.send_message(content=f">>> Command `{d_error.command}` is now on cooldown, try again in `{round(d_error.retry, 1)}s`.", ephemeral=True)
+		except Exception as e:
+			print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+			traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+			self.trace_error("get_command_error", e)
+
+	@commands.Cog.listener("on_app_command_error")
+	async def get_app_command_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+		"""App command Error Handler
+		doc: https://discordpy.readthedocs.io/en/master/interactions/api.html#exception-hierarchy
+		"""
+		try:
+			await self.__respond_to_interaction(interaction)
+			edit = interaction.edit_original_response #interaction.edit_original_message
+
+			raise error
+		except app_commands.CommandInvokeError as d_error:
+			if isinstance(d_error.original, discord.errors.InteractionResponded):
+				await edit(content=f"{d_error.original}")
+			elif isinstance(d_error.original, discord.errors.Forbidden):
+				await edit(content=f"`{type(d_error.original).__name__}` : {d_error.original.text}")
+			else:
+				self.trace_error("get_view_error", d_error)
+				await edit(content=f"`{type(d_error.original).__name__}` : {d_error.original}")
+		except app_commands.CheckFailure as d_error:
+			if isinstance(d_error, app_commands.errors.CommandOnCooldown):
+				await edit(content=f"Command is on cooldown, wait `{str(d_error).split(' ')[7]}` !")
+			else:
+				await edit(content=f"`{type(d_error).__name__}` : {d_error}")
+		except app_commands.CommandNotFound:
+			await edit(content=f"Command was not found.. Seems to be a discord bug, probably due to desynchronization.\nMaybe there is multiple commands with the same name, you should try the other one.")
+		except Exception as e: 
+			"""
+			Caught here:
+			app_commands.TransformerError
+			app_commands.CommandLimitReached
+			app_commands.CommandAlreadyRegistered
+			app_commands.CommandSignatureMismatch
+			"""
+			#print('Ignoring exception in command {}:'.format(interaction.command), file=sys.stderr)
+			#traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+			self.trace_error("get_app_command_error", e)
+
+	@commands.Cog.listener("on_view_error")
+	async def get_view_error(self, interaction: discord.Interaction, error: Exception, item: any):
+		"""View Error Handler"""
+		try:
+			raise error
+		except discord.errors.Forbidden:
+			pass
+		except Exception as e:
+			self.trace_error("get_view_error", e)
+
+	@commands.Cog.listener("on_modal_error")
+	async def get_modal_error(self, interaction: discord.Interaction, error: Exception):
+		"""Modal Error Handler"""
+		try:
+			raise error
+		except discord.errors.Forbidden:
+			pass
+		except Exception as e:
+			self.trace_error("get_modal_error", e)
+
+async def setup(bot: PIBot):
+	await bot.add_cog(Errors(bot))
