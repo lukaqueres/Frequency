a7f5a3496262a3b4d958abeace4b6ad5c2cb782f
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-17 21:54:05 +0200

    Update utilities.py

--- bot/packets/utilities.py
+++ bot/packets/utilities.py
@@ -44,6 +44,7 @@ class Logger:
 		self.addTimestamp = self.configuration.read(category="utilities", key="log.add_timestamp")
 		
 	def hard(self, text) -> None:	
+		print('hardlog')
 		text = (self.time.str_todaytime() + ' ' if self.addTimestamp else '') + '>>> ' + text
 		if self.print:
 			print(text);
