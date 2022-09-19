c8a3fad72e70699196f02875ff9ebbef8f68d622
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-19 18:46:56 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -27,7 +27,7 @@ async def startup():
 			if cog.endswith('.py'):	 # - Every file from directory path with .py extension is handled as a potential cog. -
 				if not cog[:-3] in self.configuration.read(category="utilities", key="developer.extensions.ignore"): # - If cog is not ignored ( in ignored list ) -
 					try: # - Try to load extensions, allow to load extensions with error without abort -
-						await client.load_extension(f"{self.configuration.read(category="utilities", key="developer.extensions.directory")}.{cog[:-3]}");
+						await client.load_extension(f"{self.configuration.read(category='utilities', key='developer.extensions.directory')}.{cog[:-3]}");
 						loaded.append(cog[:-3]);
 					except Exception as e: # - Catch exception in loading, can be extended. -
 						failed.append([cog[:-3], getattr(e, 'message', repr(e))]);
