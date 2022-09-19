862a8b591f0ed361fb6a7473b4cb9c164e075e71
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-19 11:25:27 +0200

    Update events.py

--- bot/cogs/events.py
+++ bot/cogs/events.py
@@ -14,26 +14,9 @@ class Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_guild_join(self, guild):
-		with open('./configuration.json', 'r') as c: # - Open 'configuration.json' json file. Getting logging details. -
-			configuration = json.load(c); 
-			log = configuration['developer']['log'];
-			defaults = configuration['values']['defaults'];
-		if log['notices']:
-			print(f'Joined guild: {guild.name}; {guild.id}');
-		members = len([m for m in guild.members if not m.bot]); # - Get members count excluding bots. -
-		time = Time();
-		roles = {};
-		for r in guild.roles:
-			if r != guild.default_role and not r.managed:
-				#roles[r.id] = self.client.database.escape.string(r.name); # - Adapting string to don't cause errors while inputting to DB. TODO: Do something to indicate that it was addapted. -
-				roles[r.id] = r.name;
-		payload = { "id": guild.id,
-			"prefix": defaults['prefix'], # - TODO/DONE/: Check what to do to input string containing ' or ", then maybe add name field -
-			"language": defaults['language'],
-			"roles": roles,
-		};
-		self.client.database.insert(table = 'guilds.properties',
-					    payload = payload);
+		self.client.log.notify(f'Joined guild: {guild.name}; {guild.id}');
+		self.client.database.predefined.add_new_guild(guild);
+		
 	@commands.Cog.listener()
 	async def on_guild_remove(self, guild):
 		with open('./configuration.json', 'r') as c: # - Open 'configuration.json' json file. Getting logging details. -
