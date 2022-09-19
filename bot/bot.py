3ad437d52fdb2d2c873faac8b2cc2c60cc029386
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-19 17:56:07 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,23 +1,14 @@
-# - Importing required unspecified packages -
-import json, os, random, asyncio, traceback, sys
-from random import randrange, randint
+# - Importing required exeternal packages -
+import os, asyncio
 
 # - Importing discord packages -
 import discord
-from discord.ext import commands, tasks
-from discord import Intents, app_commands
+from discord import app_commands
 
 
 # - Importing in-project packages -
-from packets.database import Database
 from packets.discord import PIBot
 
-# - Import cog as a part of slash not-sync work-around -
-#from cogs.configuration import ConfigurationGroup
-
-
-intents = discord.Intents.all() # - Get all Intents TODO: Remember to get messages and other permissions that require discord approval after verification -
-# bot = client = commands.Bot(command_prefix = prefix, intents=intents); # - Old client setup, moved to custom class instead -
 client = PIBot()
 
 # >---------------------------------------< COMMANDS >---------------------------------------< # 
@@ -29,29 +20,26 @@ async def ping(interaction: discord.Interaction):
 
 # >---------------------------------------< COGS / EXTENSIONS LOAD >---------------------------------------< # 
 async def startup():
-	if self.configuration.read(category="utilities", key="developer.extensions.load"):
+	if self.configuration.read(category="utilities", key="developer.extensions.load"): # - If load extensions (cogs)-
 		loaded = []; # - Extensions loaded succesfully -
 		failed = []; # - Extensions failed to load -
 		for cog in os.listdir(self.configuration.read(category="utilities", key="developer.extensions.directory")):
-			if cog.endswith('.py'):	 # - Every file from directory path with .py extension is threated as cog. -
-				if not cog[:-3] in self.configuration.read(category="utilities", key="developer.extensions.ignore"):
-					try:
+			if cog.endswith('.py'):	 # - Every file from directory path with .py extension is handled as a potential cog. -
+				if not cog[:-3] in self.configuration.read(category="utilities", key="developer.extensions.ignore"): # - If cog is not ignored ( in ignored list ) -
+					try: # - Try to load extensions, allow to load extensions with error without abort -
 						await client.load_extension(f"{self.configuration.read(category="utilities", key="developer.extensions.directory")}.{cog[:-3]}");
 						loaded.append(cog[:-3]);
 					except Exception as e: # - Catch exception in loading, can be extended. -
 						failed.append([cog[:-3], getattr(e, 'message', repr(e))]);
 				else:
-					failed.apped([cog[:-3], 'Extension ignored.']);
-		if len(loaded) != 0:
+					failed.apped([cog[:-3], 'Extension ignored.']); # - Ignored cog -
+		if len(loaded) != 0: # - If there is no loaded not show -
 			client.log.notify(f"Extensions loaded ({len(loaded)}): {', '.join(str(l) for l in loaded)}"); # - Log loaded cogs with it's number and list. -
-		if len(failed) != 0:
+		if len(failed) != 0: # - Show only if there is anything to show -
 			client.log.notify(f"Failed to load ({len(failed)}) extensions: {', '.join(str(f[0] + ': ' + f[1]) for f in failed)}"); # - Log failed cogs with it's number and list. -
 		
-		async with client:
-			TOKEN = os.environ.get('TOKEN')
-			await client.start(TOKEN)
+	async with client: 
+		TOKEN = os.environ.get('TOKEN')
+		await client.start(TOKEN) # - Just run this BITCH -
 
 asyncio.run(startup());
-#if __name__ == "__main__":
-#	client.run(TOKEN)
-		
