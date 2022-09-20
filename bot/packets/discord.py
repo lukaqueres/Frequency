faf2c888bb7398a141288d92c2018f77e3fd7514
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-20 18:48:22 +0200

    Update discord.py

--- bot/packets/discord.py
+++ bot/packets/discord.py
@@ -158,7 +158,7 @@ class PIBot(commands.Bot): # discord.Client
 	
 	#@util.dependencies("cogs.tickets.TicketView") # - not working -
 	def __sync_views(self):
-		from cogs.tickets import ticketView
+		from cogs.tickets import TicketView
 		
 		if not self.synced_views:
 			self.add_view(ticketView())
