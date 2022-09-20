b9e9356983796d69369b05380003cc74c6c23a1c
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-20 22:13:25 +0300

    Update tickets.py

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -49,9 +49,7 @@ class TicketCloseConfirmView(discord.ui.View):
 		
 	@discord.ui.button(label = "Abort", style = discord.ButtonStyle.grey, custom_id = "abort_ticket_closure_button")
 	async def abort_close_ticket_button(self, interaction: discord.Interaction, button: discord.ui.button):
-		print(f"INTERACTION: {interaction.__dict__}")
-		print(f"INTERACTION.MESSAGE: {interaction.message.__dict__}")
-		await interaction.message.delete()
+		await button.view.stop()
 		await interaction.response.send_message("Ticket closure aborted. Disaffirming all actions.", ephemeral = True)
 
 class TicketManageView(discord.ui.View):
