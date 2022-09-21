0b38b7181f250e69c14e642b102de494bf03daa2
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-21 21:17:27 +0200

    Update tickets.py

--- bot/views/tickets.py
+++ bot/views/tickets.py
@@ -8,15 +8,15 @@ from typing import Optional
 
 from packets.discord import PIBot, PIEmbed
 
-class TicketLaunchView(discord.ui.View):
+class TicketFunctions:
 	def __init__(self) -> None:
-		super().__init__(timeout = None)
-
-	@discord.ui.button(label = "Create ticket", style = discord.ButtonStyle.blurple, custom_id = "create_ticket_button")
-	async def create_ticket_button(self, interaction: discord.Interaction, button: discord.ui.button):
+		pass;
+	
+	async def create_ticket(self, interaction: discord.Interaction, for_member: Optional[discord.Member] = None) -> None:
+		member = for_member or interaction.user
 		await interaction.response.send_message("Creating ticket...", ephemeral = True)
 		ticketPrefix = "ticket"
-		ticket = utils.get(interaction.guild.text_channels, name=f"{ticketPrefix}-{interaction.user.name}-{interaction.user.discriminator}")
+		ticket = utils.get(interaction.guild.text_channels, name=f"{ticketPrefix}-{member.name.lower().replace(' ', '-')}-{member.discriminator}")
 		if ticket is not None: return await interaction.edit_original_response(content = f"There is a ticket opened already for you in {ticket.mention} channel. Write your message there.", ephemeral = True);
 		else:
 			pass
@@ -24,20 +24,29 @@ class TicketLaunchView(discord.ui.View):
 		ticketModerator = interaction.guild.get_role(moderatorId)
 		overwrites = {
 			interaction.guild.default_role: discord.PermissionOverwrite(view_channel = False),
-			interaction.user: discord.PermissionOverwrite(view_channel = True, read_message_history = True, send_messages = True, attach_files = True, embed_links = True),
+			member: discord.PermissionOverwrite(view_channel = True, read_message_history = True, send_messages = True, attach_files = True, embed_links = True),
 			interaction.guild.me: discord.PermissionOverwrite(view_channel = True, send_messages = True, read_message_history = True),
 			ticketModerator: discord.PermissionOverwrite(view_channel = True, read_message_history = True, send_messages = True, attach_files = True, embed_links = True)
 		}
-		try: channel = await interaction.guild.create_text_channel(name = f"{ticketPrefix}-{interaction.user.name}-{interaction.user.discriminator}", overwrites = overwrites, category = interaction.channel.category, reason = f"As a ticket for user {interaction.user.name} #{interaction.user.discriminator}")
+		try: channel = await interaction.guild.create_text_channel(name = f"{ticketPrefix}-{member.name}-{member.discriminator}", overwrites = overwrites, category = interaction.channel.category, reason = f"As a ticket for user {member.name} #{member.discriminator}")
 		except: return await interaction.edit_original_response(content = "Ticket creation failed, please check bot permissions", ephemeral = True)
-		ping = await channel.send(f">>> Ticket with: {interaction.user.mention}, {ticketModerator.mention}.");
+		ping = await channel.send(f">>> Ticket with: {member.mention}, {ticketModerator.mention}.");
 		await ping.delete()
-		title = f"Ticket with {interaction.user.name}"
+		title = f"Ticket with {member.name}"
 		description = "Here you can talk to staff without disturbing"
 		embed = PIEmbed(title = title, description = description)
 		embed.timestamp = None
 		await channel.send(embed = embed, view = TicketManageView());
 		await interaction.edit_original_response(content = f">>> Your ticket's channel has been created here: {channel.mention}")
+
+class TicketLaunchView(discord.ui.View):
+	def __init__(self) -> None:
+		super().__init__(timeout = None)
+		self.functions = TicketFunctions()
+
+	@discord.ui.button(label = "Create ticket", style = discord.ButtonStyle.blurple, custom_id = "create_ticket_button")
+	async def create_ticket_button(self, interaction: discord.Interaction, button: discord.ui.button):
+		await self.functions.create_ticket(interaction = interaction, for_member = interaction.user)
 		#except Exception as error:
 		#	traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 			
