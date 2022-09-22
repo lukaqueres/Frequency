3c2edea34ee2afe0e3e774ecb442478f8489723e
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-22 21:24:59 +0200

    Update tickets.py

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -7,8 +7,63 @@ from discord.app_commands.checks import has_permissions, cooldown
 from typing import Optional
 
 from packets.discord import PIBot, PIEmbed
+from packets.database import Database
+from packets.utilities import Configuration
+
 from views.tickets import TicketFunctions, TicketLaunchView, TicketManageView, TicketCloseConfirmView	
 	
+class Ticket:
+	def __init__(self) -> None:
+		self.database = Database()
+		self.configuration = Configuration()
+		
+	def __create_overwrites(self, interaction) -> dict:
+		moderatorRoles = []
+		moderatorRights = discord.PermissionOverwrite(view_channel = True, read_message_history = True, send_messages = True, attach_files = True, embed_links = True)
+		overwrites = {
+			interaction.guild.default_role: discord.PermissionOverwrite(view_channel = False),
+			interaction.user: discord.PermissionOverwrite(view_channel = True, read_message_history = True, send_messages = True, attach_files = True, embed_links = True),
+			interaction.guild.me: discord.PermissionOverwrite(view_channel = True, send_messages = True, read_message_history = True),
+		}
+		for moderator in moderatorRoles:
+			overwrites[moderator] = moderatorRights
+		return overwrites
+		
+	async def create(self, interaction: discord.Interaction, for_member: Optional[discord.Member] = None) -> None:
+		enabled = self.database.select(table = 'guilds.tickets', columns = [ 'enabled' ], condition = { "guild_id": interaction.guild.id });
+		if not enabled:
+			return await interaction.response.send_message("Tickets in this guild are currently disabled.", ephemeral = True)
+		member = for_member or interaction.user
+		await interaction.response.send_message("Creating ticket...", ephemeral = True)
+		ticketPrefix = "ticket"
+		ticket = utils.get(interaction.guild.text_channels, name=f"{ticketPrefix}-{member.name.lower().replace(' ', '-')}-{member.discriminator}")
+		if ticket is not None: return await interaction.edit_original_response(content = f"There is a ticket opened already for you in {ticket.mention} channel. Write your message there.", ephemeral = True);
+		else:
+			pass
+		moderatorId = 1020060418871414824
+		ticketModerator = interaction.guild.get_role(moderatorId)
+		
+		try: channel = await interaction.guild.create_text_channel(name = f"{ticketPrefix}-{member.name}-{member.discriminator}", overwrites = overwrites, category = interaction.channel.category, reason = f"As a ticket for user {member.name} #{member.discriminator}")
+		except: return await interaction.edit_original_response(content = "Ticket creation failed, please check bot permissions", ephemeral = True)
+		ping = await channel.send(f">>> Ticket with: {member.mention}, {ticketModerator.mention}.");
+		await ping.delete()
+		title = f"Ticket with {member.name}"
+		description = "Here you can talk to staff without disturbing"
+		embed = PIEmbed(title = title, description = description)
+		embed.timestamp = None
+		await channel.send(embed = embed, view = TicketManageView());
+		await interaction.edit_original_response(content = f">>> Your ticket's channel has been created here: {channel.mention}")
+	
+	async def close(self, interaction: discord.Interaction) -> None:
+		ticketPrefix = "ticket"
+		if (ticketPrefix + '-') in interaction.channel.name:
+			title = "Confirm ticket's closure"
+			description = "After confirmation ticket will be closed with channel removed"
+			embed = PIEmbed(title = title, description = description)
+			embed.timestamp = None
+			await interaction.response.send_message(embed = embed, view = TicketCloseConfirmView(), ephemeral = True)
+		else:
+			await interaction.response.send_message("Current channel is not a ticket", ephemeral = True)
 class Tickets(commands.Cog): #app_commands.Group
 	def __init__(self, client: PIBot) -> None:
 		super().__init__()
