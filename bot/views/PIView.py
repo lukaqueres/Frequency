1f771a27d66cdc0fe5f6c03b5cfaf1de96b9bdd0
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-22 09:27:52 +0200

    Update PIView.py

--- bot/views/PIView.py
+++ bot/views/PIView.py
@@ -11,5 +11,5 @@ from packets.database import Database
 
 class PIView(discord.ui.View):
 	def __init__(self, timeout: Optional[float] = None) -> None:
-		super().__init__(timeout)
+		super().__init__(self, timeout)
 		self.database = Database()
