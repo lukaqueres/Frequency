231bdb2445b961213f64f39b689cb7a734ad21e8
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-22 09:50:13 +0200

    Update tickets.py

--- bot/views/tickets.py
+++ bot/views/tickets.py
@@ -16,7 +16,7 @@ class TicketFunctions:
 		pass;
 	
 	async def create_ticket(self, interaction: discord.Interaction, for_member: Optional[discord.Member] = None) -> None:
-		enabled = self.database.select(table = 'guilds.tickets', columns = [ 'enabled' ], condition = { "id": interaction.guild.id });
+		enabled = self.database.select(table = 'guilds.tickets', columns = [ 'enabled' ], condition = { "guild_id": interaction.guild.id });
 		if not enabled:
 			return await interaction.response.send_message("Tickets in this guild are currently disabled.", ephemeral = True)
 		member = for_member or interaction.user
