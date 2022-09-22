863fa56d773ca9835d82c03c2202fc171167f42d
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-22 09:21:37 +0200

    Create PIView.py

---
+++ bot/views/PIView.py
@@ -0,0 +1,15 @@
+import discord, datetime, os, traceback, sys
+
+from discord.ext import commands
+from discord import app_commands, utils
+from discord.app_commands.checks import has_permissions, cooldown
+
+from typing import Optional
+
+from packets.discord import PIEmbed
+from packets.database import Database
+
+class PIView(discord.ui.View):
+	def __init__(self, timeout: Optional[float] = None) -> None:
+		super().__init__(timeout)
+		self.database = Database()
