32f4265f432fad9034d4e318de14081050e2547c
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-27 21:34:32 +0200

    Update PIView.py

--- bot/views/PIView.py
+++ bot/views/PIView.py
@@ -13,3 +13,11 @@ class PIView(discord.ui.View):
 	def __init__(self, timeout : Optional[float] = None) -> None:# : Optional[float] 
 		super().__init__(timeout = timeout)
 		self.database = Database()
+
+class PITextInput(discord.ui.TextInput):
+	def __init__(self, **kwargs):
+        	# The placeholder is what will be shown when no option is chosen
+        	# The min and max values indicate we can only pick one of the three options
+        	# The options parameter defines the dropdown options. We defined this above
+        	#super().__init__(label = "Reason", style=discord.TextStyle.short, custom_id = "ticket_reason_of_close_input", placeholder="Input reason for ticket close", default=None, required=False, min_length=0, max_length=100, row=2)
+        	super().__init__(**kwargs)	
