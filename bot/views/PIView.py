7cd1577b731f8b2634a3e82f6710aa1bdf20414e
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-27 21:52:18 +0200

    Update PIView.py

--- bot/views/PIView.py
+++ bot/views/PIView.py
@@ -19,8 +19,8 @@ class PITextInput(discord.ui.TextInput):
         	# The placeholder is what will be shown when no option is chosen
         	# The min and max values indicate we can only pick one of the three options
         	# The options parameter defines the dropdown options. We defined this above
-		#super().__init__(label = "Reason", style=discord.TextStyle.short, custom_id = "ticket_reason_of_close_input", placeholder="Input reason for ticket close", default=None, required=False, min_length=0, max_length=100, row=2)
-		super().__init__(**kwargs)
+		super().__init__(label = "Reason", style=discord.TextStyle.short, custom_id = "ticket_reason_of_close_input", placeholder="Input reason for ticket close", default=None, required=False, min_length=0, max_length=100, row=2)
+		#super().__init__(**kwargs)
 	
 	async def callback(self, interaction: discord.Interaction):
         # Use the interaction object to send a response message containing
