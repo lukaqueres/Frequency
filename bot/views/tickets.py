490e2a36b796b0dc971cd40c55b5dbeadccf581a
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-28 21:33:00 +0200

    Update tickets.py

--- bot/views/tickets.py
+++ bot/views/tickets.py
@@ -10,25 +10,19 @@ from packets.discord import PIBot, PIEmbed
 
 from views.PIView import PIView, PITextInput
 	
-class Dropdown(discord.ui.Select):
+class TextInput(discord.ui.TextInput):
 	def __init__(self):
-		options = [
-			discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
-			discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
-			discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦'),
-		]
-
         	# The placeholder is what will be shown when no option is chosen
         	# The min and max values indicate we can only pick one of the three options
         	# The options parameter defines the dropdown options. We defined this above
-		super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=options)
+		super().__init__(label = "Reason", style=discord.TextStyle.short, custom_id = "ticket_reason_of_close_input", placeholder="Input reason for ticket close", min_length=0, max_length=100, row=1)
 
 	async def callback(self, interaction: discord.Interaction):
         # Use the interaction object to send a response message containing
         # the user's favourite colour or choice. The self object refers to the
         # Select object, and the values attribute gets a list of the user's
         # selected options. We only want the first one.
-		await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
+		await interaction.response.send_message(f'Your reason is {self.value}')
 	
 class TicketConsoleView(PIView):
 	def __init__(self) -> None:
@@ -70,7 +64,7 @@ class TicketCloseConfirmView(PIView):
 	def __init__(self) -> None:
 		super().__init__(timeout = 300.0)
 		#self.add_item(PITextInput())
-		self.add_item(Dropdown())
+		self.add_item(TextInput())
 
 	@discord.ui.button(label = "Confirm", style = discord.ButtonStyle.red)
 	async def confirm_close_ticket_button(self, interaction: discord.Interaction, button: discord.ui.button):
