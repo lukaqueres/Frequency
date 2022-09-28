42eaba5d3b869c3983a9f449b58f3678ae278b02
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-28 12:55:43 +0200

    Create information.py

---
+++ bot/cogs/information.py
@@ -0,0 +1,13 @@
+import discord
+
+from discord import app_command 
+
+class Information(commands.Cog): #app_commands.Group
+	def __init__(self, client: PIBot) -> None:
+		super().__init__()
+		self.client = client
+	
+	info = app_commands.Group(name="info", description="Display information & debug data of various objects")
+		
+async def setup(client: PIBot) -> None:
+	await client.add_cog(Information(client)) 
