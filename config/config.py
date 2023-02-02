ba2e316938a613efb74e32feac55b42913ee174f
lukaqueres <lkocieln@gmail.com>
2023-02-02 21:53:11 +0100

    Changed spaces to tabs

--- config/config.py
+++ config/config.py
@@ -4,64 +4,64 @@ from typing import Any
 
 
 class Configuration:
-    """
-    Example of usage:
+	"""
+	Example of usage:
 
-    >>> configuration = Configuration("example")
+	>>> configuration = Configuration("example")
 
-    >>> configuration.get("example", "my.statement")
-    'I like games'
-    >>> print("About: ", configuration.get("example", "about_this_file"))
-    About:  This is a test file for testing config.py file ( Specifically loading JSON values part )
-    """
-    def __init__(self, *files):
-        """
-        Class for storing config data usage
+	>>> configuration.get("example", "my.statement")
+	'I like games'
+	>>> print("About: ", configuration.get("example", "about_this_file"))
+	About:  This is a test file for testing config.py file ( Specifically loading JSON values part )
+	"""
+	def __init__(self, *files):
+		"""
+		Class for storing config data usage
 
-        @param *files: Names of JSON files with configuration
-        """
-        self.file_template = "{}.json"
-        self.saved = {}
-        for name in files:
-            self.__fetch(name)
+		@param *files: Names of JSON files with configuration
+		"""
+		self.file_template = "{}.json"
+		self.saved = {}
+		for name in files:
+			self.__fetch(name)
 
-    # noinspection PyTypeChecker
-    def __fetch(self, name: str) -> None:
-        """Fetches content of JSON file and load in to parameter
+	# noinspection PyTypeChecker
+	def __fetch(self, name: str) -> None:
+		"""Fetches content of JSON file and load in to parameter
 
-        @type name: str
-        @param name: Name of configuration file to load, also name for category
-        """
+		@type name: str
+		@param name: Name of configuration file to load, also name for category
+		"""
 
-        file = self.file_template.format(name)
-        try:
-            with open(file, 'r') as c:
-                content = json.load(c)
-        except Exception as error:
-            raise error
-        else:
-            self.saved[name] = content
+		file = self.file_template.format(name)
+		try:
+			with open(file, 'r') as c:
+				content = json.load(c)
+		except Exception as error:
+			raise error
+		else:
+			self.saved[name] = content
 
-    # noinspection PyTypeChecker
-    def get(self, category: str, key: str) -> Any:
-        """ Get provided key from selected category
+	# noinspection PyTypeChecker
+	def get(self, category: str, key: str) -> Any:
+		""" Get provided key from selected category
 
-        @type category: str
-        @param category: Name of category
-        @type key: str
-        @param key: Key of specified value
+		@type category: str
+		@param category: Name of category
+		@type key: str
+		@param key: Key of specified value
 
-        @return value: Returns value under provided key
-        """
-        config = self.saved[category]
-        keys = key.split('.')
-        try:
-            for key in keys:
-                config = config[key]
-        except Exception as error:
-            raise error
-        else:
-            return config
+		@return value: Returns value under provided key
+		"""
+		config = self.saved[category]
+		keys = key.split('.')
+		try:
+			for key in keys:
+				config = config[key]
+		except Exception as error:
+			raise error
+		else:
+			return config
 
 
 doctest.run_docstring_examples(Configuration, globals())
--- database/db.py
+++ database/db.py
@@ -7,37 +7,37 @@ from typing import Any
 
 
 class Database:
-    """
-        Example of usage:
+	"""
+		Example of usage:
 
-        >>> db = Database("example")
+		>>> db = Database(os.environ.get('DATABASE_URL'))
 
-        >>> db.get("example", "my.statement")
-        'I like games'
-        >>> print("About: ", db.get("example", "about_this_file"))
-        About:  This is a test file for testing config.py file ( Specifically loading JSON values part )
-        """
-    def __init__(self):
-        self.con = psycopg2.connect(os.environ.get('DATABASE_URL'), sslmode='require')
-        self.cur = self.con.cursor()
+		//>>> db.select("example", "my.statement")
+		'I like games'
+		//>>> print("About: ", db.get("example", "about_this_file"))
+		About:  This is a test file for testing config.py file ( Specifically loading JSON values part )
+		"""
+	def __init__(self, url: str):
+		self.con = psycopg2.connect(url)
+		self.cur = self.con.cursor()
 
-    def select(self, table: str, columns: str | list, *constraints) -> Any:
-        query = "SELECT %(columns)s FROM %(table)s WHERE %(constraints)s"
-        data = {'columns': columns, 'table': table, 'constraints': constraints}
-        self.cur(query, data)
-        return self.cur.fetchall()
+	def select(self, table: str, columns: str | list, *constraints) -> Any:
+		query = "SELECT %(columns)s FROM %(table)s WHERE %(constraints)s"
+		data = {'columns': columns, 'table': table, 'constraints': constraints}
+		self.cur(query, data)
+		return self.cur.fetchall()
 
-    def exists(self):
-        pass
+	def exists(self):
+		pass
 
-    def insert(self):
-        pass
+	def insert(self, table: str, values: dict) -> dict | bool:
+		pass
 
-    def update(self):
-        pass
+	def update(self):
+		pass
 
-    def delete(self):
-        pass
+	def delete(self):
+		pass
 
 
 doctest.run_docstring_examples(Database, globals())
