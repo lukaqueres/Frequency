6243ac6a4febbe1acaadce0bcb3c9525adc070fb
lukaqueres <lkocieln@gmail.com>
2023-04-03 22:28:00 +0200

    Polish `param` & `Converter` working

--- packages/database/Query.py
+++ packages/database/Query.py
@@ -1,7 +1,8 @@
 import re
 import functools
+import logging
 
-from __decors import param, Converter, convert
+from __decors import param, Converter
 
 from typing import TypeVar
 from typing import Optional
@@ -10,6 +11,8 @@ from typing import Any
 from psycopg2 import sql
 
 from Errors import InvalidColumnGiven
+
+logger = logging.getLogger('database')
 
 TQuery = TypeVar("TQuery", bound="Query")
 
@@ -28,8 +31,13 @@ class Result:
 
 
 @Converter
-def select_converter(self):
-	pass
+def select_converter(*columns):
+	return columns
+
+
+@Converter
+def distinct_converter():
+	return True
 
 
 class Query:
@@ -39,20 +47,14 @@ class Query:
 	def __init__(self, table: str):
 		self.__table = table
 
-		self.__distinct = False
-		self.__limit = 0
-		self.__order = ""
-
-		self.__columns = {}
-
 		self.__where = []
 
 	def where(self, *args) -> TQuery:
 		self.__where.append(list(args))
 		return self
 
+	@param(converter=distinct_converter)
 	def distinct(self) -> TQuery:
-		self.__distinct = True
 		return self
 
 	@param(default="*", converter=select_converter)
@@ -62,8 +64,8 @@ class Query:
 	def ordered_by(self, column: str, method: Optional[str] = "desc") -> TQuery:
 		pass
 
+	@param
 	def take(self, limit: int) -> TQuery:
-		self.__limit = limit
 		return self
 
 	def get(self) -> Result:
